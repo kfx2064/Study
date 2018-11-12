262c39a2e986f28c30518f0fc9eb800f0f53b3ca
TaeHyun Jeong <jth454@naver.com>
2018-11-13 05:59:35 +0900

    study

--- SpringBoot1/boot08/src/main/java/org/zerock/security/SecurityConfig.java
+++ SpringBoot1/boot08/src/main/java/org/zerock/security/SecurityConfig.java
@@ -1,15 +1,24 @@
 package org.zerock.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import lombok.extern.java.Log;
 
 @Log
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	DataSource dataSource;
 	
 	@Autowired
 	ZerockUsersService zerockUsersService;
@@ -36,6 +45,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		//http.userDetailsService(zerockUsersService);
 		
 		http.rememberMe().key("zerock").userDetailsService(zerockUsersService);
+		
+		http.rememberMe()
+	    .key("zerock")
+	    .userDetailsService(zerockUsersService)
+	    .tokenRepository(getJDBCRepository())
+		.tokenValiditySeconds(60 * 60 * 24);
+		
+	}
+	
+	private PersistentTokenRepository getJDBCRepository() {
+		
+		JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+		repo.setDataSource(dataSource);
+		return repo;
 		
 	}
 
