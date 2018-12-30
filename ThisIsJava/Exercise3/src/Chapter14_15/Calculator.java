12dd3e2bd6c8a4ef11fbfd63acd8ca444afc0bf2
TaeHyun Jeong <jth454@naver.com>
2018-12-30 20:30:06 +0900

    study

---
+++ ThisIsJava/Exercise3/src/Chapter14_15/Calculator.java
@@ -0,0 +1,13 @@
+package Chapter14_15;
+
+public class Calculator {
+	
+	public static int staticMethod(int x, int y) {
+		return x + y;
+	}
+	
+	public int intstanceMethod(int x, int y) {
+		return x + y;
+	}
+
+}
