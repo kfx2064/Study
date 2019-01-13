ed62459d4fe500ac171e2f7e5c2a98d3ac2d2e88
TaeHyun Jeong <jth454@naver.com>
2019-01-13 16:19:33 +0900

    study

--- SpringBootLectureExercise01/Springinit/src/main/java/me/whiteship/springinit/SpringinitApplication.java
+++ SpringBootLectureExercise01/Springinit/src/main/java/me/whiteship/springinit/SpringinitApplication.java
@@ -1,16 +1,20 @@
 package me.whiteship.springinit;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import me.whiteship.Holoman;
 
 @SpringBootApplication
 public class SpringinitApplication {
 
 	public static void main(String[] args) {
-		
+
 		SpringApplication application = new SpringApplication(SpringinitApplication.class);
+		application.setWebApplicationType(WebApplicationType.NONE);
 		application.run(args);
-		
 		
 		//SpringApplication.run(SpringinitApplication.class, args);
 	}
