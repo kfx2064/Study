23168ec83b3650033fd4998fe082b98b871dd509
unknown <jth454@naver.com>
2019-01-19 16:21:30 +0900

    study

---
+++ SpringBootLectureExercise01/springbootexception/src/main/java/me/whiteship/SpringbootexceptionApplication.java
@@ -0,0 +1,14 @@
+package me.whiteship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootexceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootexceptionApplication.class, args);
+	}
+
+}
+
