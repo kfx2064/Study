4a586b8adb1674af727a9b600983045ec6b56fd3
unknown <jth454@naver.com>
2019-01-19 16:22:09 +0900

    study

---
+++ SpringBootLectureExercise01/springbootmvc/src/test/java/me/whiteship/springbootmvc/SampleControllerTest.java
@@ -0,0 +1,50 @@
+package me.whiteship.springbootmvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
+public class SampleControllerTest {
+
+	@Autowired
+	MockMvc mockMvc;
+	
+	@Autowired
+	WebClient webClient;
+	
+	@Test
+	public void hello() throws Exception {
+		HtmlPage page = webClient.getPage("/hello");
+		HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+		assertThat(h1.getTextContent()).isEqualToIgnoringCase("taehyun");
+		
+		/*
+		mockMvc.perform(get("/hello"))
+				.andExpect(status().isOk())
+				.andDo(print())				
+				.andExpect(view().name("hello"))
+				.andExpect(model().attribute("name", is("taehyun")))
+				.andExpect(content().string(containsString("taehyun")));
+				*/
+	}
+	
+}
