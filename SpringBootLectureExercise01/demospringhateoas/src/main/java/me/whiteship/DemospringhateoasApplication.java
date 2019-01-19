6bff0d53b427d90d3998910735787246e073087d
unknown <jth454@naver.com>
2019-01-19 16:19:41 +0900

    study

---
+++ SpringBootLectureExercise01/demospringhateoas/src/main/java/me/whiteship/DemospringhateoasApplication.java
@@ -0,0 +1,14 @@
+package me.whiteship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringhateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringhateoasApplication.class, args);
+	}
+
+}
+
