8a7d45e1cf24fb1a652ec0637fe75899102557f5
unknown <jth454@naver.com>
2019-02-10 17:40:40 +0900

    study

---
+++ StandardProcedureOfJava/StandardProcedureOfJava01/src/Chapter14_4/LambdaEx3.java
@@ -0,0 +1,35 @@
+package Chapter14_4;
+
+@FunctionalInterface
+interface MyFunction {
+	void myMethod();
+}
+
+class Outer {
+	int val = 10;		// Outer.this.val
+	
+	class Inner {
+		int val = 20;		// this.val
+		
+		void method(int i) {		// void method(final int i)
+			int val = 30;
+			
+			MyFunction f = () -> {
+				System.out.println("i : " + i);
+				System.out.println("val : " + val);
+				System.out.println("this.val : " + ++this.val);
+				System.out.println("Outer.this.val : " + ++Outer.this.val);
+			};
+			
+			f.myMethod();
+		}
+	}
+}
+
+public class LambdaEx3 {
+	public static void main(String[] args) {
+		Outer outer = new Outer();
+		Outer.Inner inner = outer.new Inner();
+		inner.method(100);
+	}
+}
