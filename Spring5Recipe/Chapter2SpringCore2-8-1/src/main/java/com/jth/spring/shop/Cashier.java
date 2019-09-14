c8a4f6b8b4c17ef0e64fb9ac2827d44d645d85bb
unknown <jth454@naver.com>
2019-09-14 23:42:16 +0900

    Studying.

---
+++ Spring5Recipe/Chapter2SpringCore2-8-1/src/main/java/com/jth/spring/shop/Cashier.java
@@ -0,0 +1,60 @@
+package com.jth.spring.shop;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Date;
+
+public class Cashier {
+	
+	private String fileName;
+	private String path;
+	private BufferedWriter writer;
+	
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public BufferedWriter getWriter() {
+		return writer;
+	}
+	public void setWriter(BufferedWriter writer) {
+		this.writer = writer;
+	}
+	
+	public void openFile() throws IOException {
+		
+		File targetDir = new File(path);
+		if(!targetDir.exists()) {
+			targetDir.mkdir();
+		}
+		
+		File checkoutFile = new File(path, fileName + ".txt");
+		if(!checkoutFile.exists()) {
+			checkoutFile.createNewFile();
+		}
+		
+		writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(checkoutFile, true)));
+		
+	}
+	
+	public void checkout(ShoppingCart cart) throws IOException {
+		writer.write(new Date() + "\t" + cart.getItems() + "\r\n");
+		writer.flush();
+	}
+	
+	public void closeFile() throws IOException {
+		writer.close();
+	}
+
+}
