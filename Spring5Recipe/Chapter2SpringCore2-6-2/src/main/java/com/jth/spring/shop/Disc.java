5abfe18f4dfbf6480d3507d2ecbf7e90ce4015d3
unknown <jth454@naver.com>
2019-09-14 15:18:53 +0900

    Study.

---
+++ Spring5Recipe/Chapter2SpringCore2-6-2/src/main/java/com/jth/spring/shop/Disc.java
@@ -0,0 +1,23 @@
+package com.jth.spring.shop;
+
+public class Disc extends Product {
+	
+	private int capacity;
+	
+	public Disc() {
+		super();
+	}
+	
+	public Disc(String name, double price, double discount) {
+		super(name, price, discount);
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	
+}
