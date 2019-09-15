323e6ed0c7103243893d98974dd2a185136b796b
unknown <jth454@naver.com>
2019-09-15 19:18:22 +0900

    Studying.

---
+++ Spring5Recipe/Chapter2SpringCore2-9-4/src/main/java/com/jth/spring/sequence/DatePrefixGenerator.java
@@ -0,0 +1,21 @@
+package com.jth.spring.sequence;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DatePrefixGenerator implements PrefixGenerator {
+	
+	private DateFormat formatter;
+
+	public void setPattern(String pattern) {
+		this.formatter = new SimpleDateFormat(pattern);
+	}
+	
+	@Override
+	public String getPrefix() {
+		String result = formatter.format(new Date());
+		return result;
+	}
+
+}
