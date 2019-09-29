a3b6b922a69b16673b0ce0f29e4fd98db2face32
unknown <jth454@naver.com>
2019-09-29 15:57:34 +0900

    studying

---
+++ Spring5Recipe2/Chapter2SpringCore2-13-5/src/main/java/com/jth/spring/calculator/UnitCalculatorImpl.java
@@ -0,0 +1,24 @@
+package com.jth.spring.calculator;
+
+import org.springframework.stereotype.Component;
+
+@Component("unitCalculator")
+public class UnitCalculatorImpl implements UnitCalculator {
+
+	@Override
+	public double kilogramToPound(double kilogram) {
+		double result = 0.0;
+		result = kilogram * 2.2;
+		System.out.println(kilogram + " kilogram = " + result + " pound");
+		return result;
+	}
+
+	@Override
+	public double kilometerToMile(double kilometer) {
+		double result = 0.0;
+		result = kilometer * 0.62;
+		System.out.println(kilometer + " kilometer = " + result + " mile");
+		return result;
+	}
+	
+}
