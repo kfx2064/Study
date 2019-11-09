9e802e1137089ad77a4cf7bacc75d2174686737f
jth45 <jth454@naver.com>
2019-11-09 10:59:27 +0900

    removed

--- Spring5Recipe4/Chapter7SpringCore7-3-1/build/resources/main/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE todo (
-    id                  bigint          AUTO_INCREMENT PRIMARY KEY
-    , owner              VARCHAR(255)    NOT NULL
-    , description        VARCHAR(255)    NOT NULL
-    , completed         BOOLEAN         NOT NULL DEFAULT false
-);
