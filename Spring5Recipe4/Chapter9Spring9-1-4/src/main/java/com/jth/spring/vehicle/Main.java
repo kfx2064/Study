76ed18ac441ddb3b3f0297767646cc12d940b1ad
unknown <jth454@naver.com>
2019-11-16 18:10:38 +0900

    studying

---
+++ Spring5Recipe4/Chapter9Spring9-1-4/src/main/java/com/jth/spring/vehicle/Main.java
@@ -0,0 +1,20 @@
+package com.jth.spring.vehicle;
+
+import com.jth.spring.vehicle.config.VehicleConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        ApplicationContext context = new AnnotationConfigApplicationContext(VehicleConfiguration.class);
+
+        VehicleDao vehicleDao = context.getBean(VehicleDao.class);
+        Vehicle vehicle = new Vehicle("TEM0014", "Red", 4, 4);
+        vehicleDao.insert(vehicle);
+
+        vehicle = vehicleDao.findByVehicleNo("TEM0014");
+        System.out.println(vehicle);
+    }
+
+}
