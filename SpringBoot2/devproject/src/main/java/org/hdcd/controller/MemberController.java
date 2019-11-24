276295745c516b8996fe795727d57fe17a20d480
unknown <jth454@naver.com>
2019-11-24 11:42:00 +0900

    spring boot training.

--- SpringBoot2/devproject/src/main/java/org/hdcd/controller/MemberController.java
+++ SpringBoot2/devproject/src/main/java/org/hdcd/controller/MemberController.java
@@ -1,12 +1,16 @@
 package org.hdcd.controller;
 
+import org.hdcd.dto.Member;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
 
 @Controller
 public class MemberController {
@@ -18,6 +22,41 @@ public class MemberController {
         logger.info("registerForm");
 
         return "registerForm";
+    }
+
+    @RequestMapping(value = "/registerByGet01", method = RequestMethod.GET)
+    public String registerByGet01(String userId, @DateTimeFormat(pattern = "yyyyMMdd") Date dateOfBirth) {
+        logger.info("registerByGet01");
+
+        logger.info("userId = " + userId);
+
+        logger.info("dateOfBirth = " + dateOfBirth);
+
+        return "success";
+    }
+
+    @RequestMapping(value = "/registerByGet02", method = RequestMethod.GET)
+    public String registerByGet02(Member member) {
+        logger.info("registerByGet02");
+
+        logger.info("member user id : " + member.getUserId());
+
+        logger.info("member date of birth : " + member.getDateOfBirth());
+
+        return "success";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String register(Member member) {
+        logger.info("register");
+
+        logger.info("member user id : " + member.getUserId());
+
+        logger.info("member password : " + member.getPassword());
+
+        logger.info("member date of birth : " + member.getDateOfBirth());
+
+        return "success";
     }
 
     @RequestMapping(value = "/register/{userId}", method = RequestMethod.GET)
@@ -41,10 +80,41 @@ public class MemberController {
     }
 
     @RequestMapping(value = "/register01", method = RequestMethod.POST)
-    public String register01(String userId) {
+    public String register01(Member member) {
         logger.info("register01");
 
-        logger.info("userId = " + userId);
+        logger.info("member userid = " + member.getUserId());
+        logger.info("member password = " + member.getPassword());
+
+        return "success";
+    }
+
+    @RequestMapping(value = "/register02", method = RequestMethod.POST)
+    public String register02(Member member, int coin) {
+        logger.info("register02");
+
+        logger.info("member getuserid : " + member.getUserId());
+
+        logger.info("member get password : " + member.getPassword());
+
+        logger.info("member coin : " + member.getCoin());
+
+        logger.info("coin : " + coin);
+
+        return "success";
+    }
+
+    @RequestMapping(value = "/register03", method = RequestMethod.POST)
+    public String register03(int uid, Member member) {
+        logger.info("register03");
+
+        logger.info("uid = " + uid);
+
+        logger.info("member user id : " + member.getUserId());
+
+        logger.info("member password : " + member.getPassword());
+
+        logger.info("member coin : " + member.getCoin());
 
         return "success";
     }
--- SpringBoot2/devproject/src/main/java/org/hdcd/dto/Member.java
+++ SpringBoot2/devproject/src/main/java/org/hdcd/dto/Member.java
@@ -1,10 +1,22 @@
 package org.hdcd.dto;
 
-public class Member {
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Member implements Serializable {
 
     private String userName = "JeongTaeHyun";
 
     private String password = "1234";
+
+    private String userId = "jeongtaehyun id";
+
+    private int coin = 100;
+
+    @DateTimeFormat(pattern = "yyyyMMdd")
+    private Date dateOfBirth;
 
     public String getUserName() {
         return userName;
@@ -20,5 +32,29 @@ public class Member {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public int getCoin() {
+        return coin;
+    }
+
+    public void setCoin(int coin) {
+        this.coin = coin;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
     }
 }
