75701d8fb8ebc27ae4a6c333cd2a7d25827d05a8
ActualSituation <44404918+ActualSituation@users.noreply.github.com>
2019-11-26 21:58:44 +0900

    Service, Mapper made.

--- SpringBoot2/ImageShop06DBConfig/imageshop06dbconfig/src/main/java/com/jth/service/CodeClassServiceImpl.java
+++ SpringBoot2/ImageShop06DBConfig/imageshop06dbconfig/src/main/java/com/jth/service/CodeClassServiceImpl.java
@@ -1,4 +1,38 @@
 package com.jth.service;
 
-public class CodeClassServiceImpl {
+import com.jth.domain.CodeClass;
+import com.jth.mapper.CodeClassMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CodeClassServiceImpl implements CodeClassService {
+
+    @Autowired
+    private CodeClassMapper mapper;
+
+    @Override
+    public void create(CodeClass codeClass) throws Exception {
+        mapper.create(codeClass);
+    }
+
+    @Override
+    public CodeClass read(String classCode) throws Exception {
+        return mapper.read(classCode);
+    }
+
+    @Override
+    public void update(CodeClass codeClass) throws Exception {
+        mapper.update(codeClass);
+    }
+
+    @Override
+    public void delete(String classCode) throws Exception {
+        mapper.delete(classCode);
+    }
+
+    @Override
+    public List<CodeClass> list() throws Exception {
+        return mapper.list();
+    }
 }
