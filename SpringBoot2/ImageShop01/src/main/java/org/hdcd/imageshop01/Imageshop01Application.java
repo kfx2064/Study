b795dbb10908a7f606c85d61b134710aa6783c03
unknown <jth454@naver.com>
2019-11-26 16:07:17 +0900

    start new project

---
+++ SpringBoot2/ImageShop01/src/main/java/org/hdcd/imageshop01/Imageshop01Application.java
@@ -0,0 +1,13 @@
+package org.hdcd.imageshop01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Imageshop01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Imageshop01Application.class, args);
+    }
+
+}
