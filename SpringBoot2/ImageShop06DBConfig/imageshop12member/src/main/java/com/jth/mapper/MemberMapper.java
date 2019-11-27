fa71c511879a20ce9afa6dcf71aa3648de571483
ActualSituation <44404918+ActualSituation@users.noreply.github.com>
2019-11-27 12:28:07 +0900

    middle stage

--- SpringBoot2/ImageShop06DBConfig/imageshop12member/src/main/java/com/jth/mapper/MemberMapper.java
+++ SpringBoot2/ImageShop06DBConfig/imageshop12member/src/main/java/com/jth/mapper/MemberMapper.java
@@ -1,4 +1,28 @@
 package com.jth.mapper;
 
+import com.jth.domain.Member;
+import com.jth.domain.MemberAuth;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface MemberMapper {
+
+    public Member readByUserId(String userId);
+
+    public void create(Member member) throws Exception;
+
+    public Member read(int userNo) throws Exception;
+
+    public void update(Member member) throws Exception;
+
+    public void delete(int userNo) throws Exception;
+
+    public List<Member> list() throws Exception;
+
+    public void createAuth(MemberAuth memberAuth) throws Exception;
+
+    public void deleteAuth(int userNo) throws Exception;
+
 }
