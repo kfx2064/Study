f8c59f61eeb544c5806ec7153391f6a32aa5a486
ActualSituation <44404918+ActualSituation@users.noreply.github.com>
2019-11-27 18:37:20 +0900

    middle stage

---
+++ SpringBoot2/ImageShop06DBConfig/imageshop12member/src/main/java/com/jth/mapper/BoardMapper.java
@@ -0,0 +1,10 @@
+package com.jth.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BoardMapper {
+
+
+
+}
---
+++ SpringBoot2/ImageShop06DBConfig/imageshop12member/src/main/java/com/jth/service/BoardService.java
@@ -0,0 +1,4 @@
+package com.jth.service;
+
+public interface BoardService {
+}
