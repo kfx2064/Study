b670e43aa7151e5e48f72cac8439227fe8b34dc7
ActualSituation <44404918+ActualSituation@users.noreply.github.com>
2019-11-27 13:41:30 +0900

    done

--- SpringBoot2/ImageShop06DBConfig/imageshop12member/src/main/java/com/jth/config/SecurityConfig.java
+++ SpringBoot2/ImageShop06DBConfig/imageshop12member/src/main/java/com/jth/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.jth.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,7 +27,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
+    @Bean
     public PasswordEncoder createPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
