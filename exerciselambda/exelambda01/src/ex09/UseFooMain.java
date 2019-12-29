f013ff4a54b47e66b5eb3437438ad2c529882812
ActualSituation <44404918+ActualSituation@users.noreply.github.com>
2019-12-29 11:14:23 +0900

    'study.'

--- exerciselambda/exelambda01/src/ex09/UseFooMain.java
+++ exerciselambda/exelambda01/src/ex09/UseFooMain.java
@@ -1,4 +1,28 @@
 package ex09;
 
+import java.util.function.Function;
+
 public class UseFooMain {
+
+    public static void main(String[] args) throws Exception {
+
+        UseFoo useFoo = new UseFoo();
+
+        Foo foo = parameter -> parameter + " from lamba";
+
+        String result = useFoo.add("Message", foo);
+
+        System.out.println("result : " + result);
+
+        Function<String, String> fn = parameter -> parameter + " from lambda";
+        String resultFn = add("Message", fn);
+
+        System.out.println("result after : " + resultFn);
+
+    }
+
+    public static String add(String string, Function<String, String> fn) {
+        return fn.apply(string);
+    }
+
 }
