5427547b0ee6bf8e7a69e1ef59a8e54a1f11c70f
ActualSituation <44404918+ActualSituation@users.noreply.github.com>
2019-12-29 11:12:11 +0900

    'other'

--- JavaBasicExercise/BasicLecture01/src/ex01/ExceptionEx5.java
+++ JavaBasicExercise/BasicLecture01/src/ex01/ExceptionEx5.java
@@ -1,4 +1,23 @@
 package ex01;
 
 public class ExceptionEx5 {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(1);
+        System.out.println(2);
+
+        try {
+
+            System.out.println(3);
+            System.out.println(0/0);
+            System.out.println(4);
+
+        } catch (ArithmeticException e) {
+            System.out.println(5);
+        }
+        System.out.println(6);
+
+    }
+
 }
--- JavaBasicExercise/BasicLecture01/src/ex01/ExceptionEx7.java
+++ JavaBasicExercise/BasicLecture01/src/ex01/ExceptionEx7.java
@@ -1,4 +1,35 @@
 package ex01;
 
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+
 public class ExceptionEx7 {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(1);
+        System.out.println(2);
+
+        try {
+            System.out.println(3);
+            System.out.println(0/0);
+            System.out.println(4);
+        } catch (ArithmeticException ae) {
+            if(ae instanceof ArithmeticException) {
+                System.out.println("true");
+            }
+
+            if(ae instanceof ArithmeticException) {
+                System.out.println("instanceof ae");
+            }
+
+            System.out.println("ArithmeticException");
+
+        } catch (Exception e) {
+            System.out.println("Exception");
+        }
+
+        System.out.println(6);
+
+    }
+
 }
