13232545d2185dddd2b189e3d183ee4213625d44
ActualSituation <44404918+ActualSituation@users.noreply.github.com>
2020-01-16 21:40:09 +0900

    rest api events implements

---
+++ inflearn-rest-api/src/test/java/com/jth/restapi/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jth.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
