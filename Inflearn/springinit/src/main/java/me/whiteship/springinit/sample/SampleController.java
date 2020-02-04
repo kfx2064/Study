ae391c1a35e59e14265008e5b516df2a5a4a81b6
ActualSituation <44404918+ActualSituation@users.noreply.github.com>
2020-02-04 18:39:26 +0900

    study inflearn.

--- Inflearn/springinit/src/main/java/me/whiteship/springinit/sample/SampleController.java
+++ Inflearn/springinit/src/main/java/me/whiteship/springinit/sample/SampleController.java
@@ -1,4 +1,19 @@
 package me.whiteship.springinit.sample;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SampleController {
+
+    @Autowired
+    private SampleService sampleService;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello" + sampleService.getName();
+    }
+
 }
