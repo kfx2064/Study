c11670e8b96919d095ce4dbfa7da96a671ee55e0
ActualSituation <44404918+ActualSituation@users.noreply.github.com>
2020-02-04 16:22:06 +0900

    training spring boot.

--- Inflearn/demospringmvc/src/test/java/me/whiteship/demospringmvc/SampleControllerTest.java
+++ Inflearn/demospringmvc/src/test/java/me/whiteship/demospringmvc/SampleControllerTest.java
@@ -1,4 +1,32 @@
 package me.whiteship.demospringmvc;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
 public class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("name", "keesun"))
+                .andExpect(content().string(containsString("keesun")));
+    }
+
 }
