55e423f74de9d7cc0b743cebf39f31ccfd3b9023
ActualSituation <44404918+ActualSituation@users.noreply.github.com>
2020-03-01 18:00:49 +0900

    Bad request.

--- inflearn-rest-api/src/main/java/com/jth/restapi/events/EventDto.java
+++ inflearn-rest-api/src/main/java/com/jth/restapi/events/EventDto.java
@@ -2,21 +2,33 @@ package com.jth.restapi.events;
 
 import lombok.*;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Builder @NoArgsConstructor @AllArgsConstructor
 @Getter @Setter
 public class EventDto {
 
+    @NotEmpty
     private String name;
+    @NotNull
     private String description;
+    @NotNull
     private LocalDateTime beginEnrollmentDateTime;
+    @NotNull
     private LocalDateTime closeEnrollmentDateTime;
+    @NotNull
     private LocalDateTime beginEventDateTime;
+    @NotNull
     private LocalDateTime endEventDateTime;
     private String location;    // optional
+    @Min(0)
     private int basePrice;      // optional
+    @Min(0)
     private int maxPrice;       // optional
+    @Min(0)
     private int limitOfEnrollment;
 
 }
