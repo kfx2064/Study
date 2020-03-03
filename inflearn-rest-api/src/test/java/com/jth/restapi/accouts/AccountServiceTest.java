8cd144984bbabda27abe7d01120c7eb363c2d098
unknown <jth454@naver.com>
2020-03-03 10:30:59 +0900

    account service test case.

--- inflearn-rest-api/src/test/java/com/jth/restapi/accouts/AccountServiceTest.java
+++ inflearn-rest-api/src/test/java/com/jth/restapi/accouts/AccountServiceTest.java
@@ -1,22 +1,30 @@
 package com.jth.restapi.accouts;
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Autowired
     AccountService accountService;
@@ -45,6 +53,21 @@ public class AccountServiceTest {
         System.out.println(userDetails.toString());
         // Then
         assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+    @Test
+    public void findByUsernameFail() {
+        String username = "random@email.com";
+
+        // Expected
+        // Expected를 이용할 때는 실제 로직보다 앞에 위치해야 한다.
+        // 뒤에 위치하면 제대로 동작하지 않을 수도 있다.
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        // When
+        accountService.loadUserByUsername(username);
+
     }
 
 }
