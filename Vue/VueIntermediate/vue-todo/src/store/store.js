018a93a22be591af9aed291ee127d7e1a1f6fa76
ActualSituation <44404918+ActualSituation@users.noreply.github.com>
2020-06-03 17:03:30 +0900

    vue 2 round end.

--- Vue/VueIntermediate/vue-todo/src/store/store.js
+++ Vue/VueIntermediate/vue-todo/src/store/store.js
@@ -1,57 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import todoApp from "./modules/todoApp";
+// import * as getters from "./getters";
+// import * as mutations from "./mutations";
 
 Vue.use(Vuex);
 
-const storage = {
-  fetch() {
-    const arr = [];
-    if (localStorage.length > 0) {
-      for (let i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
-          arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-        }
-      }
-    }
-    return arr;
-  }
-};
-
 export const store = new Vuex.Store({
-  state: {
-    todoItems: storage.fetch()
-  },
-  getters: {
-    storedTodoItems(state) {
-      return state.todoItems;
-    }
-  },
-  mutations: {
-    addOneItem(state, todoItem) {
-      console.log("received");
-      const obj = { completed: false, item: todoItem };
-      localStorage.setItem(todoItem, JSON.stringify(obj));
-      //this.todoItems.push(obj);
-      state.todoItems.push(obj);
-    },
-    removeOneItem(state, payload) {
-      console.log("received");
-      localStorage.removeItem(payload.todoItem.item);
-      state.todoItems.splice(payload.index, 1);
-    },
-    toggleOneItem(state, payload) {
-      console.log("toggleOneItem");
-      state.todoItems[payload.index].completed = !state.todoItems[payload.index]
-        .completed;
-      localStorage.removeItem(payload.todoItem.item);
-      localStorage.setItem(
-        payload.todoItem.item,
-        JSON.stringify(payload.todoItem)
-      );
-    },
-    clearAllItems(state) {
-      localStorage.clear();
-      state.todoItems = [];
-    }
+  modules: {
+    todoApp
   }
 });
