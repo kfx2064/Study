47d586e370d04ecdd793fc2559242646bcb9685c
unknown <jth454@naver.com>
2020-08-21 17:29:19 +0900

    training spring boot rest api with jpa.

--- SpringBootRestJPAAPI/devproject/src/main/java/org/hdcd/controller/MemberController.java
+++ SpringBootRestJPAAPI/devproject/src/main/java/org/hdcd/controller/MemberController.java
@@ -2,13 +2,17 @@ package org.hdcd.controller;
 
 import lombok.extern.java.Log;
 import org.hdcd.domain.Member;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Log
 @RestController
@@ -50,6 +54,21 @@ public class MemberController {
         list.add(member2);
 
         return list.toString();
+    }
+
+    @RequestMapping(value = "/register05", method = RequestMethod.GET)
+    public ResponseEntity<Map<String, Object>> register05() {
+        log.info("register05");
+
+        Map<String, Object> map = new HashMap<>();
+
+        Member member = new Member();
+        map.put("key1", member.toString());
+
+        Member member2 = new Member();
+        map.put("key2", member2.toString());
+
+        return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
     }
 
 }
