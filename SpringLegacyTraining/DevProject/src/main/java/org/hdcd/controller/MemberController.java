2e88a22ae3a64f8a4c9f2160ff329f7a6cf9475c
unknown <jth454@naver.com>
2020-08-25 17:19:08 +0900

    training spring mvc.

--- SpringLegacyTraining/DevProject/src/main/java/org/hdcd/controller/MemberController.java
+++ SpringLegacyTraining/DevProject/src/main/java/org/hdcd/controller/MemberController.java
@@ -835,4 +835,94 @@ public class MemberController {
         return "read05";
     }
 
+    @RequestMapping(value = "/read06", method = RequestMethod.GET)
+    public String read06(Model model) {
+        logger.info("read06");
+
+        Member member = new Member();
+
+        member.setUserId("hongkd");
+        member.setPassword("1234");
+        member.setEmail("aaa@ccc.com");
+        member.setUserName("홍길동");
+        member.setBirthDay("1989-09-07");
+
+        member.setGender("female");
+
+        member.setDeveloper("Y");
+        member.setForeigner(true);
+
+        member.setNationality("Australia");
+
+        member.setCars("saab");
+
+        String[] carArray = {"saab", "audi"};
+
+        member.setCarArray(carArray);
+
+        List<String> carList = new ArrayList<>();
+        carList.add("saab");
+        carList.add("audi");
+        member.setCarList(carList);
+
+        member.setHobby("Movie");
+
+        String[] hobbyArray = {"Music", "Movie"};
+
+        member.setHobbyArray(hobbyArray);
+
+        List<String> hobbyList = new ArrayList<>();
+        hobbyList.add("Music");
+        hobbyList.add("Movie");
+
+        member.setHobbyList(hobbyList);
+
+        Address address = new Address();
+        address.setPostCode("080908");
+        address.setLocation("seoul");
+
+        member.setAddress(address);
+
+        List<Card> cardList = new ArrayList<>();
+
+        Card card1 = new Card();
+        card1.setNo("123456");
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, 2020);
+        cal.set(Calendar.MONTH, 9);
+        cal.set(Calendar.DAY_OF_MONTH, 8);
+
+        card1.setValidMonth(cal.getTime());
+
+        cardList.add(card1);
+
+        Card card2 = new Card();
+        card2.setNo("456786");
+
+        cal.set(Calendar.YEAR, 2022);
+        cal.set(Calendar.MONTH, 11);
+        cal.set(Calendar.DAY_OF_MONTH, 7);
+
+        card2.setValidMonth(cal.getTime());
+
+        cardList.add(card2);
+
+        member.setCardList(cardList);
+
+        cal.set(Calendar.YEAR, 1988);
+        cal.set(Calendar.MONTH, 10);
+        cal.set(Calendar.DAY_OF_MONTH, 7);
+
+        member.setDateOfBirth(cal.getTime());
+
+        String introduction = "안녕하세요.\n반갑습니다.";
+
+        member.setIntroduction(introduction);
+
+        model.addAttribute("user", member);
+
+        return "read05";
+    }
+
 }
--- SpringLegacyTraining/DevProject/src/main/webapp/WEB-INF/views/read05.jsp
+++ SpringLegacyTraining/DevProject/src/main/webapp/WEB-INF/views/read05.jsp
@@ -8,22 +8,55 @@
 <body>
 <h3>Result</h3>
 
-<h4>user.address : </h4>
-user.address.postCode : ${user.address.postCode} <br>
-user.address.location : ${user.address.location} <br>
+user.userId:    ${user.userId}<br>
+user.password:  ${user.password}<br>
+user.userName:  ${user.userName}<br>
+user.email:     ${user.email}<br>
+user.birthDay:  ${user.birthDay}<br>
+user.gender:    ${user.gender}<br>
+user.hobby:     ${user.hobby}<br>
 
-<h4>user.cardList : </h4>
+<c:forEach var="hobby" items="${user.hobbyArray}">
+    <c:out value="${hobby}" /><br>
+</c:forEach>
+
+<c:forEach var="hobby" items="${user.hobbyList}">
+    <c:out value="${hobby}" /><br>
+</c:forEach>
+
+user.foreigner:     ${user.foreigner}<br>
+user.developer:     ${user.developer}<br>
+user.nationality:   ${user.nationality}<br>
+
+user.address.postCode:  ${user.address.postCode}<br>
+user.address.location:  ${user.address.location}<br>
+
 <c:forEach var="card" items="${user.cardList}">
     <c:out value="${card.no} ${card.validMonth}" /><br>
 </c:forEach>
 
 <c:forEach var="card" items="${user.cardList}">
-    <c:out value="${card.no}" /><br>
-    <c:out value="${card.validMonth}" /><br>
+    <c:out value="${card.no}"/><br>
+    <c:out value="${card.validMonth}"/><br>
 </c:forEach>
 
 <c:forEach var="card" items="${user.cardList}">
-    ${card.no} ${card.validMonth}
+    ${card.no} ${card.validMonth} <br>
 </c:forEach>
+
+user.cars : ${user.cars} <br>
+
+<c:forEach var="car" items="${user.carArray}">
+    <c:out value="${car}"/><br>
+</c:forEach>
+
+<c:forEach var="car" items="${user.carList}">
+    <c:out value="${car}"/><br>
+</c:forEach>
+
+user.introduction : ${user.introduction} <br>
+
+user.dateOfBirth : ${user.dateOfBirth}
+
 </body>
 </html>
