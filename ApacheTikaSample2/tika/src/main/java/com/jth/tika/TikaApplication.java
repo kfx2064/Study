4a819c9912e49cbbcae6ab254b7111c853d9eccd
unknown <jth454@naver.com>
2020-08-27 09:19:12 +0900

    apache tika middle save.

---
+++ ApacheTikaSample2/tika/src/main/java/com/jth/tika/TikaApplication.java
@@ -0,0 +1,13 @@
+package com.jth.tika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TikaApplication.class, args);
+    }
+
+}
