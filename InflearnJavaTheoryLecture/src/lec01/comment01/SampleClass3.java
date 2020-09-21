8bb7a54c0ce7cc3599f81b6d875fc19538e74d8c
unknown <jth454@naver.com>
2020-09-21 17:36:26 +0900

    made lecture.

---
+++ InflearnJavaTheoryLecture/src/lec01/comment01/SampleClass3.java
@@ -0,0 +1,30 @@
+package lec01.comment01;
+
+/**
+ * 샘플 클래스
+ * author : jth
+ * date : 2020.09.21.
+ */
+public class SampleClass3 {
+
+    /**
+     * 메인 메소드
+     * author : jth
+     * date : 2020.09.21.
+     * @param args
+     */
+    public static void main(String[] args) {
+        // 프린트 메소드 실행
+        printMethod();
+    }
+
+    /**
+     * 데이터 프린팅
+     * author : jth
+     * date : 2020.09.21.
+     */
+    public static void printMethod() {
+        System.out.println("프린팅 기능입니다.");
+    }
+    
+}
