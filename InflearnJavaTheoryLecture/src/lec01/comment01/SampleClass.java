31c66fb1010904df2c390e5c041f6df6ed16e50b
unknown <jth454@naver.com>
2020-09-21 17:31:13 +0900

    java theory lecture.

---
+++ InflearnJavaTheoryLecture/src/lec01/comment01/SampleClass.java
@@ -0,0 +1,19 @@
+package lec01.comment01;
+
+/**
+ * <pre>
+ *     클래스 주석
+ * </pre>
+ */
+public class SampleClass {
+
+    public static void main(String[] args) {
+        /*
+            블록 주석
+         */
+        System.out.println("Hello, world.");
+        //System.out.println("세계야, 안녕.");
+        // 라인 주석
+    }
+
+}
