3f85b1f4e45628a5207cba9d0c41068c7d257bf4
unknown <jth454@naver.com>
2020-09-21 17:56:36 +0900

    java theory added.

---
+++ InflearnJavaTheoryLecture/src/lec01/datatype02/DataType01.java
@@ -0,0 +1,42 @@
+package lec01.datatype02;
+
+public class DataType01 {
+
+    public static void main(String[] args) {
+
+        char charOne = '안';
+        char charTwo = '녕';
+
+        System.out.print(charOne);
+        System.out.println(charTwo);
+
+        String strOne = "안녕하세요.";
+        System.out.println(strOne);
+
+        byte byteOne = 1;
+        System.out.println("byteOne : " + byteOne);
+
+        short shortOne = 30;
+        System.out.println("shortOne : " + shortOne);
+
+        int intOne = 20;
+        System.out.println("intOne : " + intOne);
+
+        long longOne = 100;
+        System.out.println("longOne : " + longOne);
+
+        float floatOne = 1.1f;
+        System.out.println("floatOne : " + floatOne);
+
+        double doubleOne = 2.3;
+        System.out.println("doubleOne : " + doubleOne);
+
+        boolean booleanOne = true;
+        System.out.println("booleanOne : " + booleanOne);
+
+        boolean booleanTwo = false;
+        System.out.println("booleanTwo : " + booleanTwo);
+
+    }
+
+}
