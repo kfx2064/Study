6eab9384045c84b5785a2aad58ea8a7cd7bc2f5b
unknown <jth454@naver.com>
2020-11-29 18:23:49 +0900

    make lecture.

--- InflearnJavaTheoryLecture/src/lec04/OOP15/second/Korea.java
+++ InflearnJavaTheoryLecture/src/lec04/OOP15/second/Korea.java
@@ -1,4 +1,18 @@
 package lec04.OOP15.second;
 
 public class Korea extends Country {
+
+    public String special = "분단국가며 휴전국가이다.";
+
+    public void showDefault() {
+        String species = super.species;
+        String haveMust = super.haveMust;
+        System.out.println("종 : " + species);
+        System.out.println("필요한 것 : " + haveMust);
+    }
+
+    @Override
+    public void mainEat() {
+        System.out.println("밥과 김치를 먹는다.");
+    }
 }
---
+++ InflearnJavaTheoryLecture/src/lec04/OOP15/second/Main.java
@@ -0,0 +1,24 @@
+package lec04.OOP15.second;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Korea korea = new Korea();
+        China china = new China();
+
+        System.out.println(korea.species);
+        System.out.println(korea.haveMust);
+        System.out.println(korea.special);
+        korea.showDefault();
+        korea.mainEat();
+
+        System.out.println("\n\n");
+
+        System.out.println(china.species);
+        System.out.println(china.haveMust);
+        System.out.println(china.special);
+        china.showDefault();
+        china.mainEat();
+    }
+
+}
