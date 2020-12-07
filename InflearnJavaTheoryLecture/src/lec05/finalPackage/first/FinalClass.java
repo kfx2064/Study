cb3b32971373ce5e39472defb8aee14ee82746a1
unknown <jth454@naver.com>
2020-12-07 17:54:27 +0900

    making lecture.

---
+++ InflearnJavaTheoryLecture/src/lec05/finalPackage/first/FinalClass.java
@@ -0,0 +1,19 @@
+package lec05.finalPackage.first;
+
+/**
+ * 이번에는 간단하게 final을 쓴 것들을 만들어 보겠습니다.
+ */
+public class FinalClass {
+
+    public final int sampleInt = 1;
+    public final String sampleStr = "샘플스트링";
+
+    public final void sampleMethod() {
+        System.out.println("파이널 메서드입니다.");
+    }
+
+    /**
+     * 이 다음에 다른 것도 만들어 보겠습니다.
+     */
+
+}
---
+++ InflearnJavaTheoryLecture/src/lec05/finalPackage/second/UseFinalClass.java
@@ -0,0 +1,22 @@
+package lec05.finalPackage.second;
+
+/**
+ * 클래스에 파이널을 붙여보겠습니다.
+ */
+public final class UseFinalClass {
+
+    /**
+     * 간단하죠?
+     *
+     * 메서드도 만들어 볼까요?
+     */
+
+    public final void showMsg() {
+        System.out.println("파이널 메서드입니다.");
+    }
+
+    /**
+     * 파이널 실습은 이 정도로 하겠습니다.
+     */
+
+}
