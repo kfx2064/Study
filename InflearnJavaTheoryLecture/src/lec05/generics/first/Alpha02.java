cb43aa5013895288f1b216f70afca3d42c1d757a
unknown <jth454@naver.com>
2020-12-07 14:13:01 +0900

    making lecture.

---
+++ InflearnJavaTheoryLecture/src/lec05/generics/first/Alpha02.java
@@ -0,0 +1,11 @@
+package lec05.generics.first;
+
+/**
+ * 제네릭스 타입 T를 넣은 클래스를 만들었습니다.
+ * 이제 다시 GenericsTypeT01로 이동하여
+ * 객체를 생성하고 T 타입에 대한 실습을 해보겠습니다.
+ * @param <T>
+ */
+public class Alpha02<T> {
+
+}
---
+++ InflearnJavaTheoryLecture/src/lec05/generics/second/GenericsMethodTypeT01.java
@@ -0,0 +1,7 @@
+package lec05.generics.second;
+
+/**
+ * 가장 먼제 메인 메소드에 해당하는 클래스를 만들겠습니다.
+ */
+public class GenericsMethodTypeT01 {
+}
