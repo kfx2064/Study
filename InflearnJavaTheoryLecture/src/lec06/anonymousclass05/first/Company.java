c6ca5157bd9ddc80442139b5f50720a0e2da00c9
unknown <jth454@naver.com>
2020-12-25 13:38:06 +0900

    making lecture.

---
+++ InflearnJavaTheoryLecture/src/lec06/anonymousclass05/first/Company.java
@@ -0,0 +1,24 @@
+package lec06.anonymousclass05.first;
+
+/**
+ * 익명 클래스를 만들 준비를 하겠습니다.
+ * 변수와 생성자들을 만들겠습니다.
+ * 따라와 주세요.
+ */
+public class Company {
+
+    public String companyName;
+
+    Company() {
+
+    }
+
+    Company(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String returnCompanyName() {
+        return this.companyName;
+    }
+
+}
