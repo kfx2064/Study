c4598d9e9fa257ba8c63bbc18fefe485be2e2ef9
unknown <jth454@naver.com>
2020-12-30 12:34:10 +0900

    Edu sample.

---
+++ InflearnJavaTheoryLecture/src/lec08/threadExercise03/InputWareHouse.java
@@ -0,0 +1,67 @@
+package lec08.threadExercise03;
+
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+public class InputWareHouse extends Thread {
+
+    protected String wareHouseName = "제품창고[1]";
+    private boolean stop = false;
+    public int count = 0;
+
+    public void setStop(boolean isStop) {
+        stop = isStop;
+    }
+
+    @Override
+    public void run() {
+        while (stop == false) {
+            try {
+                Thread.sleep(2300);
+                Map<Integer, Map<String, Integer>> paramInputMap = inputWareHouse(count);
+                boolean isStoreSuccess = storeWareHouse(paramInputMap);
+                System.out.println();
+                if (isStoreSuccess) {
+                    System.out.println(wareHouseName + "   [[입고되었습니다.]]");
+                }
+                count++;
+            } catch (Exception e) {
+                System.out.println("입고 작업을 중단합니다.");
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
+    public synchronized Map<Integer, Map<String, Integer>> inputWareHouse(int count)
+            throws Exception {
+        Map<String, Integer> productMap
+                = StringFactoryProcess.productStringList.poll();
+        if (productMap != null) {
+            Map<Integer, Map<String, Integer>> paramInputMap
+                    = new HashMap<>();
+            paramInputMap.put(count, productMap);
+            return paramInputMap;
+        } else {
+            throw new Exception("제품이 없어서 입고 작업을 진행할 수 없습니다.");
+        }
+    }
+
+    public synchronized boolean storeWareHouse(Map<Integer, Map<String, Integer>> paramInputMap)
+            throws Exception {
+        boolean isSuccess = false;
+
+        if (paramInputMap != null) {
+            StringFactoryProcess.inputList.add(paramInputMap);
+            isSuccess = true;
+        } else if (paramInputMap == null){
+            throw new Exception("제품이 없어서 창고 저장 작업에 실패했습니다.");
+        } else {
+            isSuccess = false;
+        }
+        return isSuccess;
+    }
+}
--- InflearnJavaTheoryLecture/src/lec08/threadExercise03/StringFactoryProcess.java
+++ InflearnJavaTheoryLecture/src/lec08/threadExercise03/StringFactoryProcess.java
@@ -6,18 +6,22 @@ public class StringFactoryProcess {
 
     public static Queue<String> rawMaterialPlasticList = new LinkedList<>();
     public static Queue<Map<String, Integer>> productStringList = new LinkedList<>();
+    public static Queue<Map<Integer, Map<String, Integer>>> inputList = new LinkedList<>();
 
     public static void main(String[] args) {
         RawMaterialInputProcess rawMaterialInputProcess = new RawMaterialInputProcess();
         Thread rawMaterialThread = new Thread(rawMaterialInputProcess);
         ProductMakeProcess productMakeProcess = new ProductMakeProcess();
+        InputWareHouse inputWareHouse = new InputWareHouse();
 
         try {
             rawMaterialThread.setPriority(5);
             productMakeProcess.setPriority(5);
+            inputWareHouse.setPriority(5);
 
             rawMaterialThread.start();
             productMakeProcess.start();
+            inputWareHouse.start();
 
             Thread.sleep(12000);
             rawMaterialInputProcess.setStop(true);
@@ -27,6 +31,7 @@ public class StringFactoryProcess {
             System.out.println("[[[ 생산 결과 ]]]");
             System.out.println(rawMaterialPlasticList);
             System.out.println(productStringList);
+            System.out.println(inputList);
         } catch (Exception e) {
             e.printStackTrace();
         }
