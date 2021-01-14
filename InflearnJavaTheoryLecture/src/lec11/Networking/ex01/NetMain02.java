93c0549926c9a9ba7bf00ad8cb647975cab5ef19
thJeongGab <jth454@naver.com>
2021-01-14 17:43:30 +0900

    Network.

---
+++ InflearnJavaTheoryLecture/src/lec11/Networking/ex01/NetMain02.java
@@ -0,0 +1,37 @@
+package lec11.Networking.ex01;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class NetMain02 {
+
+    public static void main(String[] args) throws Exception {
+
+        //String netPath = "https://www.google.co.kr";
+        //String netPath = "https://www.naver.com";
+        String netPath = "https://www.daum.net";
+
+        URL url = new URL(netPath);
+        URLConnection urlConnection = url.openConnection();
+        InputStream inputStream = urlConnection.getInputStream();
+
+        BufferedReader bufferedReader
+                = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
+
+        while (true) {
+            String getString = bufferedReader.readLine();
+            if (getString == null) {
+                break;
+            } else {
+                System.out.println(getString);
+            }
+        }
+
+        bufferedReader.close();
+
+    }
+
+}
