fff3695b2bcac8c6011feef6ffc06c6d90ce1c15
thJeongGab <jth454@naver.com>
2021-01-18 16:59:23 +0900

    java fx.

---
+++ JavaLectureTheoryMakingFilm/InflearnJava/src/lec09/ex02/h/TestMain.java
@@ -0,0 +1,13 @@
+package lec09.ex02.h;
+
+import javafx.application.Application;
+
+public class TestMain {
+	
+	public static void main(String[] args) {
+		
+		Application.launch(MainView.class);
+		
+	}
+
+}
---
+++ JavaLectureTheoryMakingFilm/InflearnJava/src/lec09/ex02/j/TestMain.java
@@ -0,0 +1,13 @@
+package lec09.ex02.j;
+
+import javafx.application.Application;
+
+public class TestMain {
+	
+	public static void main(String[] args) {
+		
+		Application.launch(MainView.class);
+		
+	}
+
+}
