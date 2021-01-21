df1e18b3264028ab5c2103433e253052976bede5
thJeongGab <jth454@naver.com>
2021-01-21 17:24:44 +0900

    Spring legacy training.

--- SpringLegacyTraining/DevProject/src/main/java/org/hdcd/service/BoardServiceImpl.java
+++ SpringLegacyTraining/DevProject/src/main/java/org/hdcd/service/BoardServiceImpl.java
@@ -2,6 +2,7 @@ package org.hdcd.service;
 
 import org.hdcd.dao.BoardDAO;
 import org.hdcd.domain.Board;
+import org.hdcd.mapper.BoardMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,31 +12,31 @@ import java.util.List;
 public class BoardServiceImpl implements BoardService {
 
     @Autowired
-    private BoardDAO dao;
+    private BoardMapper mapper;
 
     @Override
     public void register(Board board) throws Exception {
-        dao.create(board);
+        mapper.create(board);
     }
 
     @Override
     public Board read(Integer boardNo) throws Exception {
-        return dao.read(boardNo);
+        return mapper.read(boardNo);
     }
 
     @Override
     public void modify(Board board) throws Exception {
-        dao.update(board);
+        mapper.update(board);
     }
 
     @Override
     public void remove(Integer boardNo) throws Exception {
-        dao.delete(boardNo);
+        mapper.delete(boardNo);
     }
 
     @Override
     public List<Board> list() throws Exception {
-        return dao.list();
+        return mapper.list();
     }
 
 }
