0e5de0e4f1d3c0cabb318b1c04abb0a58bbad847
thJeongGab <jth454@naver.com>
2021-05-10 11:46:31 +0900

    Spring Legacy.

--- SpringLegacyTraining/01/ImageShop01/dev_db_backup.sql
+++ SpringLegacyTraining/01/ImageShop01/dev_db_backup.sql
@@ -17,6 +17,24 @@ DROP DATABASE IF EXISTS `dev_db`;
 CREATE DATABASE IF NOT EXISTS `dev_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `dev_db`;
 
+-- 테이블 dev_db.access_log 구조 내보내기
+DROP TABLE IF EXISTS `access_log`;
+CREATE TABLE IF NOT EXISTS `access_log` (
+  `log_no` int(11) NOT NULL AUTO_INCREMENT,
+  `request_uri` varchar(200) NOT NULL,
+  `class_name` varchar(100) NOT NULL,
+  `class_simple_name` varchar(50) NOT NULL,
+  `method_name` varchar(100) NOT NULL,
+  `remote_addr` varchar(50) NOT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`log_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 테이블 데이터 dev_db.access_log:~0 rows (대략적) 내보내기
+DELETE FROM `access_log`;
+/*!40000 ALTER TABLE `access_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `access_log` ENABLE KEYS */;
+
 -- 테이블 dev_db.board 구조 내보내기
 DROP TABLE IF EXISTS `board`;
 CREATE TABLE IF NOT EXISTS `board` (
@@ -62,6 +80,8 @@ CREATE TABLE IF NOT EXISTS `code_class` (
 -- 테이블 데이터 dev_db.code_class:~0 rows (대략적) 내보내기
 DELETE FROM `code_class`;
 /*!40000 ALTER TABLE `code_class` DISABLE KEYS */;
+INSERT INTO `code_class` (`class_code`, `class_name`, `use_yn`, `reg_date`, `upd_date`) VALUES
+	('A01', '직업', 'Y', '2021-05-10 11:39:59', '2021-05-10 11:39:59');
 /*!40000 ALTER TABLE `code_class` ENABLE KEYS */;
 
 -- 테이블 dev_db.code_detail 구조 내보내기
@@ -80,6 +100,12 @@ CREATE TABLE IF NOT EXISTS `code_detail` (
 -- 테이블 데이터 dev_db.code_detail:~0 rows (대략적) 내보내기
 DELETE FROM `code_detail`;
 /*!40000 ALTER TABLE `code_detail` DISABLE KEYS */;
+INSERT INTO `code_detail` (`class_code`, `code_value`, `code_name`, `sort_seq`, `use_yn`, `reg_date`, `upd_date`) VALUES
+	('A01', '001', '응용SW엔지니어', 1, 'Y', '2021-05-10 11:44:18', '2021-05-10 11:44:18'),
+	('A01', '002', '시스템SW엔지니어', 2, 'Y', '2021-05-10 11:44:18', '2021-05-10 11:44:18'),
+	('A01', '003', '임베디드SW엔지니어', 3, 'Y', '2021-05-10 11:44:18', '2021-05-10 11:44:18'),
+	('A01', '004', '서버엔지니어', 4, 'Y', '2021-05-10 11:44:18', '2021-05-10 11:44:18'),
+	('A01', '005', '네트워크엔지니어', 5, 'Y', '2021-05-10 11:44:18', '2021-05-10 11:44:18');
 /*!40000 ALTER TABLE `code_detail` ENABLE KEYS */;
 
 -- 테이블 dev_db.item 구조 내보내기
@@ -98,6 +124,22 @@ DELETE FROM `item`;
 /*!40000 ALTER TABLE `item` DISABLE KEYS */;
 /*!40000 ALTER TABLE `item` ENABLE KEYS */;
 
+-- 테이블 dev_db.login_log 구조 내보내기
+DROP TABLE IF EXISTS `login_log`;
+CREATE TABLE IF NOT EXISTS `login_log` (
+  `log_no` int(11) NOT NULL AUTO_INCREMENT,
+  `user_no` int(5) NOT NULL,
+  `user_id` varchar(50) NOT NULL,
+  `remote_addr` varchar(50) NOT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`log_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 테이블 데이터 dev_db.login_log:~0 rows (대략적) 내보내기
+DELETE FROM `login_log`;
+/*!40000 ALTER TABLE `login_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `login_log` ENABLE KEYS */;
+
 -- 테이블 dev_db.member 구조 내보내기
 DROP TABLE IF EXISTS `member`;
 CREATE TABLE IF NOT EXISTS `member` (
@@ -111,11 +153,13 @@ CREATE TABLE IF NOT EXISTS `member` (
   `upd_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `enabled` char(1) DEFAULT '1',
   PRIMARY KEY (`user_no`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- 테이블 데이터 dev_db.member:~0 rows (대략적) 내보내기
 DELETE FROM `member`;
 /*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` (`user_no`, `user_id`, `user_pw`, `user_name`, `job`, `coin`, `reg_date`, `upd_date`, `enabled`) VALUES
+	(1, 'bongbong3', '$2a$10$ZeaVzhYJRpPfvMrJ.5lAIeHIYgRNzAKVOkI/sofoKPWS7uQd/3kNy', '봉봉이', '001', 0, '2021-05-10 11:44:46', '2021-05-10 11:44:46', '1');
 /*!40000 ALTER TABLE `member` ENABLE KEYS */;
 
 -- 테이블 dev_db.member_auth 구조 내보내기
@@ -130,6 +174,8 @@ CREATE TABLE IF NOT EXISTS `member_auth` (
 -- 테이블 데이터 dev_db.member_auth:~0 rows (대략적) 내보내기
 DELETE FROM `member_auth`;
 /*!40000 ALTER TABLE `member_auth` DISABLE KEYS */;
+INSERT INTO `member_auth` (`user_no`, `auth`) VALUES
+	(1, 'ROLE_MEMBER');
 /*!40000 ALTER TABLE `member_auth` ENABLE KEYS */;
 
 -- 테이블 dev_db.notice 구조 내보내기
@@ -146,6 +192,22 @@ CREATE TABLE IF NOT EXISTS `notice` (
 DELETE FROM `notice`;
 /*!40000 ALTER TABLE `notice` DISABLE KEYS */;
 /*!40000 ALTER TABLE `notice` ENABLE KEYS */;
+
+-- 테이블 dev_db.pay_coin_history 구조 내보내기
+DROP TABLE IF EXISTS `pay_coin_history`;
+CREATE TABLE IF NOT EXISTS `pay_coin_history` (
+  `history_no` int(11) NOT NULL AUTO_INCREMENT,
+  `user_no` int(5) NOT NULL,
+  `item_id` int(5) NOT NULL,
+  `amount` int(5) NOT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`history_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 테이블 데이터 dev_db.pay_coin_history:~0 rows (대략적) 내보내기
+DELETE FROM `pay_coin_history`;
+/*!40000 ALTER TABLE `pay_coin_history` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pay_coin_history` ENABLE KEYS */;
 
 -- 테이블 dev_db.pds 구조 내보내기
 DROP TABLE IF EXISTS `pds`;
@@ -176,6 +238,22 @@ CREATE TABLE IF NOT EXISTS `pds_attach` (
 DELETE FROM `pds_attach`;
 /*!40000 ALTER TABLE `pds_attach` DISABLE KEYS */;
 /*!40000 ALTER TABLE `pds_attach` ENABLE KEYS */;
+
+-- 테이블 dev_db.performance_log 구조 내보내기
+DROP TABLE IF EXISTS `performance_log`;
+CREATE TABLE IF NOT EXISTS `performance_log` (
+  `log_no` int(11) NOT NULL AUTO_INCREMENT,
+  `signature_name` varchar(50) NOT NULL,
+  `signature_type_name` varchar(100) NOT NULL,
+  `duration_time` int(11) DEFAULT 0,
+  `reg_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`log_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 테이블 데이터 dev_db.performance_log:~0 rows (대략적) 내보내기
+DELETE FROM `performance_log`;
+/*!40000 ALTER TABLE `performance_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `performance_log` ENABLE KEYS */;
 
 -- 테이블 dev_db.persistent_logins 구조 내보내기
 DROP TABLE IF EXISTS `persistent_logins`;
