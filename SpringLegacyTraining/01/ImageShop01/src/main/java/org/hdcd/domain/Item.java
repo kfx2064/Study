2f2100188402c708a240e8e0bacb8d352e273a86
thJeongGab <jth454@naver.com>
2021-05-10 17:49:03 +0900

    Spring Legacy.

--- SpringLegacyTraining/01/ImageShop01/src/main/java/org/hdcd/domain/Item.java
+++ SpringLegacyTraining/01/ImageShop01/src/main/java/org/hdcd/domain/Item.java
@@ -1,4 +1,85 @@
 package org.hdcd.domain;
 
-public class Item {
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Item implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -7171819535329599881L;
+
+    private Integer itemId;
+    private String itemName;
+    private Integer price;
+    private String description;
+    private MultipartFile picture;
+    private String pictureUrl;
+    private MultipartFile preview;
+    private String previewUrl;
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public MultipartFile getPicture() {
+        return picture;
+    }
+
+    public void setPicture(MultipartFile picture) {
+        this.picture = picture;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public MultipartFile getPreview() {
+        return preview;
+    }
+
+    public void setPreview(MultipartFile preview) {
+        this.preview = preview;
+    }
+
+    public String getPreviewUrl() {
+        return previewUrl;
+    }
+
+    public void setPreviewUrl(String previewUrl) {
+        this.previewUrl = previewUrl;
+    }
 }
