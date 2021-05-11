76794a853a05dc0bf6b00cf25bd579d8f7a06ada
thJeongGab <jth454@naver.com>
2021-05-11 17:57:21 +0900

    jpa

---
+++ SpringStarterJPATraining/01/DevProject/src/test/java/org/hdcd/devproject/DevProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hdcd.devproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
