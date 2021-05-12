ae2427e78e72bbb1a90d7a61d345c0063d6a7948
thJeongGab <jth454@naver.com>
2021-05-12 16:54:22 +0900

    jpa

--- SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/MemberController.java
+++ SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/MemberController.java
@@ -1,6 +1,8 @@
 package org.hdcd.devproject.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hdcd.devproject.domain.Address;
+import org.hdcd.devproject.domain.Card;
 import org.hdcd.devproject.domain.Member;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
@@ -10,10 +12,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Controller
 public class MemberController {
+
+    @GetMapping("/registerAllForm")
+    public String registerAllForm() {
+        log.info("registerAllForm");
+
+        return "registerAllForm";
+    }
 
     @GetMapping("/registerForm")
     public String registerForm() {
@@ -22,37 +33,314 @@ public class MemberController {
         return "registerForm";
     }
 
-    @GetMapping("/registerByGet01")
-    public String registerByGet01(String userId, @DateTimeFormat(pattern = "yyyyMMdd") LocalDate dateOfBirth) {
-        log.info("registerByGet01");
+    @PostMapping("/registerMemberUserId")
+    public String registerUserId(Member member) {
+        log.info("registerUserId");
+        log.info("member.getUserId() = " + member.getUserId());
 
+        return "success";
+    }
+
+    @PostMapping("/registerUser")
+    public String registerUser(Member member) {
+        log.info("registerUser");
+
+        log.info("member.getUserId() = " + member.getUserId());
+        log.info("member.getGender() = " + member.getGender());
+        log.info("member.getHobby() = " + member.getHobby());
+        log.info("member.isForeigner() = " + member.isForeigner());
+        log.info("member.getDeveloper() = " + member.getDeveloper());
+        log.info("member.getBirthDate() = " + member.getDateOfBirth());
+        log.info("member.getCars() = " + member.getCars());
+        log.info("member.getIntroduction() = " + member.getIntroduction());
+
+        String[] hobbyArray  =member.getHobbyArray();
+
+        if (hobbyArray != null) {
+            log.info("hobbyArray.length = " + hobbyArray.length);
+
+            for (int i = 0; i < hobbyArray.length; i++) {
+                log.info("hobbyArray[" + i + "] = " + hobbyArray[i]);
+            }
+        } else {
+            log.info("hobbyArray == null");
+        }
+
+        List<String> hobbyList = member.getHobbyList();
+
+        if (hobbyList != null) {
+            log.info("hobbyList.size() = " +hobbyList.size());
+
+            for (int i = 0; i < hobbyList.size(); i++) {
+                log.info("hobbyList(" + i + ") = " + hobbyList.get(i));
+            }
+        } else {
+            log.info("hobbyList == null");
+        }
+
+        Address address = member.getAddress();
+
+        if (address != null) {
+            log.info("address.getPostCode() = " + address.getPostCode());
+            log.info("address.getLocation() = " + address.getLocation());
+        } else {
+            log.info("address == null");
+        }
+
+        List<Card> cardList = member.getCardList();
+
+        if (cardList != null) {
+            log.info("cardList.size() = " + cardList.size());
+
+            for (int i = 0; i < cardList.size(); i++) {
+                Card card = cardList.get(i);
+                log.info("card.getNo() = " + card.getNo());
+                log.info("card.getValidMonth() = " + card.getValidMonth());
+            }
+        } else {
+            log.info("cardList == null");
+        }
+
+        String[] carArray = member.getCarArray();
+
+        if (carArray != null) {
+            log.info("carArray.length = " + carArray.length);
+
+            for (int i = 0; i < carArray.length; i++) {
+                log.info("carArray[" + i + "] = " + carArray[i]);
+            }
+        } else {
+            log.info("carArray == null");
+        }
+
+        List<String> carList = member.getCarList();
+
+        if (carList != null) {
+            log.info("carList.size() = " + carList.size());
+
+            for (int i = 0; i < carList.size(); i++) {
+                log.info("hobbyList(" + i + ") = " + carList.get(i));
+            }
+        } else {
+            log.info("carList == null");
+        }
+
+        return "success";
+    }
+
+    @PostMapping("/registerUserId")
+    public String registerUserId(String userId) {
+        log.info("registerUserId");
         log.info("userId = " + userId);
 
-        log.info("dateOfBirth = " + dateOfBirth);
+        return "success";
+    }
+
+    @PostMapping("/registerPassword")
+    public String registerPassword(String password) {
+        log.info("registerPassword");
+        log.info("password = " + password);
 
         return "success";
     }
 
-    @GetMapping("/registerByGet02")
-    public String registerByGet02(Member member) {
-        log.info("registerByGet02");
-
-        log.info("member.getUserId() = " + member.getUserId());
-
-        log.info("member.getDateOfBirth(){ = " + member.getDateOfBirth());
+    @PostMapping("/registerTextArea")
+    public String registerTextArea(String introduction) {
+        log.info("registerTextArea");
+        log.info("introduction = " + introduction);
 
         return "success";
     }
 
-    @PostMapping("/register")
-    public String register(Member member) {
-        log.info("register");
+    @PostMapping("/registerRadio")
+    public String registerRadio(String gender) {
+        log.info("registerRadio");
+        log.info("gender = " + gender);
 
-        log.info("member.getUserId() = " + member.getUserId());
+        return "success";
+    }
 
-        log.info("member.getPassword() = " + member.getPassword());
+    @PostMapping("/registerCheckbox01")
+    public String registerCheckbox01(String hobby) {
+        log.info("registerCheckbox01");
+        log.info("hobby = " + hobby);
 
-        log.info("member.getDateOfBirth() = " + member.getDateOfBirth());
+        return "success";
+    }
+
+    @PostMapping("/registerCheckbox02")
+    public String registerCheckbox02(String[] hobbyArray) {
+        log.info("registerCheckbox02");
+
+        if (hobbyArray != null) {
+            log.info("hobbyArray.length = " + hobbyArray.length);
+
+            for (int i = 0; i <hobbyArray.length; i++) {
+                log.info("hobbyArray[" + i + "] = " + hobbyArray[i]);
+            }
+        } else {
+            log.info("hobbyArray == null");
+        }
+
+        return "success";
+    }
+
+    @PostMapping("/registerCheckbox03")
+    public String registerCheckbox03(List<String> hobbyList) {
+        log.info("registerCheckbox03");
+
+        if (hobbyList != null) {
+            log.info("hobbyList.size() = " + hobbyList.size());
+
+            for (int i = 0; i < hobbyList.size(); i++) {
+                log.info("hobbyList(" + i + ") = " + hobbyList.get(i));
+            }
+        } else {
+            log.info("hobbyList == null");
+        }
+
+        return "success";
+    }
+
+    @PostMapping("/registerCheckbox04")
+    public String registerCheckbox04(String developer) {
+        log.info("registerCheckbox04");
+        log.info("developer = " + developer);
+
+        return "success";
+    }
+
+    @PostMapping("/registerCheckbox05")
+    public String registerCheckbox05(boolean foreigner) {
+        log.info("registerCheckbox05");
+        log.info("foreigner = " + foreigner);
+
+        return "success";
+    }
+
+    @PostMapping("/registerSelect")
+    public String registerSelect(String nationality) {
+        log.info("registerSelect");
+        log.info("nationality = " + nationality);
+
+        return "success";
+    }
+
+    @PostMapping("/registerMultipleSelect01")
+    public String registerMultipleSelect01(String cars) {
+        log.info("registerMultipleSelect01");
+        log.info("cars = " + cars);
+
+        return "success";
+    }
+
+    @PostMapping("/registerMultipleSelect02")
+    public String registerMultipleSelect02(String[] carArray) {
+        log.info("registerMultipleSelect02");
+
+        if (carArray != null) {
+            log.info("carArray.length = " + carArray.length);
+
+            for (int i = 0; i < carArray.length; i++) {
+                log.info("carArray[" + i + "] = " + carArray[i]);
+            }
+        } else {
+            log.info("carArray == null");
+        }
+
+        return "success";
+    }
+
+    @PostMapping("/registerMultipleSelect03")
+    public String registerMultipleSelect03(ArrayList<String> carList) {
+        log.info("registerMultipleSelect03");
+
+        if (carList != null) {
+            log.info("carList.size() = " + carList.size());
+
+            for (int i = 0; i < carList.size(); i++) {
+                log.info("hobbyList(" + i + ") = " + carList.get(i));
+            }
+        } else {
+            log.info("carList == null");
+        }
+
+        return "success";
+    }
+
+    @PostMapping("/registerAddress")
+    public String registerAddress(Address address) {
+        log.info("registerAddress");
+
+        if (address != null) {
+            log.info("address.getPostCode() = " + address.getPostCode());
+            log.info("address.getLocation() = " + address.getLocation());
+        } else {
+            log.info("address == null");
+        }
+
+        return "success";
+    }
+
+    @PostMapping("/registerUserAddress")
+    public String registerUserAddress(Member member) {
+        log.info("registerUserAddress");
+
+        Address address = member.getAddress();
+
+        if (address != null) {
+            log.info("address.getPostCode() = " + address.getPostCode());
+            log.info("address.getLocation() = " + address.getLocation());
+        } else {
+            log.info("address == null");
+        }
+
+        return "success";
+    }
+
+    @PostMapping("/registerUserCardList")
+    public String registerUserCardList(Member member) {
+        log.info("registerUserCardList");
+
+        List<Card> cardList = member.getCardList();
+
+        if (cardList != null) {
+            log.info("cardList.size() = " + cardList.size());
+
+            for (int i = 0; i < cardList.size(); i++) {
+                Card card = cardList.get(i);
+                log.info("card.getNo() = " + card.getNo());
+                log.info("card.getValidMonth = " + card.getValidMonth());
+            }
+        } else {
+            log.info("cardList == null");
+        }
+
+        return "success";
+    }
+
+    @PostMapping("/registerDate01")
+    public String registerDate01(LocalDate dateOfBirth) {
+        log.info("registerDate01");
+
+        if (dateOfBirth != null) {
+            log.info("dateOfBirth = " + dateOfBirth);
+        } else {
+            log.info("dateOfBirth == null");
+        }
+
+        return "success";
+    }
+
+    @PostMapping("/registerDate02")
+    public String registerDate02(@DateTimeFormat(pattern = "yyyyMMdd") LocalDate dateOfBirth) {
+        log.info("registerDate02");
+
+        if (dateOfBirth != null) {
+            log.info("dateOfBirth = " + dateOfBirth);
+        } else {
+            log.info("dateOfBirth == null");
+        }
 
         return "success";
     }
--- SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/domain/Member.java
+++ SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/domain/Member.java
@@ -4,6 +4,7 @@ import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
@@ -12,10 +13,28 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class Member {
 
-    private String userId = "hongkd";
-    private String password = "1234";
+    private String userId;
+    private String password;
     private String userName;
-    private int coin = 100;
+    private String email;
+    private String gender;
+    private String hobby;
+    private String[] hobbyArray;
+    private List<String> hobbyList;
+    private boolean foreigner;
+    private String developer;
+    private String nationality;
+
+    private Address address;
+
+    private List<Card> cardList;
+
+    private String cars;
+    private String[] carArray;
+
+    private List<String> carList;
+
+    private String introduction;
 
     @DateTimeFormat(pattern = "yyyyMMdd")
     private LocalDate dateOfBirth;
