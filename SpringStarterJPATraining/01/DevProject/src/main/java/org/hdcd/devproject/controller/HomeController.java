27a338e09cdf83d79d48e300d2e6fdc8e508518e
thJeongGab <jth454@naver.com>
2021-05-12 13:34:44 +0900

    jpa

--- SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/HomeController.java
+++ SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/HomeController.java
@@ -38,4 +38,49 @@ public class HomeController {
         return "ajaxHome";
     }
 
+    @GetMapping("/goHome0101")
+    public void home0101() {
+        log.info("home0101");
+    }
+
+    @GetMapping("/sub/goHome0102")
+    public void home0102() {
+        log.info("home0102");
+    }
+
+    @GetMapping("/goHome0201")
+    public String home0201() {
+        log.info("home0201");
+
+        return "home0201";
+    }
+
+    @GetMapping("/sub/goHome0202")
+    public String home0202() {
+        log.info("home0202");
+
+        return "home0202";
+    }
+
+    @GetMapping("/sub/goHome0203")
+    public String home0203() {
+        log.info("home0203");
+
+        return "sub/home0203";
+    }
+
+    @GetMapping("/goHome0204")
+    public String home0204() {
+        log.info("home0204");
+
+        return "redirect:/sub/goHome0205";
+    }
+
+    @GetMapping("/sub/goHome0205")
+    public String home0205() {
+        log.info("home0205");
+
+        return "/sub/home0205";
+    }
+
 }
