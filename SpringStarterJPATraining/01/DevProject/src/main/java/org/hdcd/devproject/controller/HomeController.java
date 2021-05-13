cd10706ca47e6606013e9e9ab23d55c00900fd64
thJeongGab <jth454@naver.com>
2021-05-13 09:47:36 +0900

    jpa

--- SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/HomeController.java
+++ SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/HomeController.java
@@ -22,100 +22,11 @@ import java.util.*;
 @Controller
 public class HomeController {
 
-    @ResponseBody
-    @GetMapping("/goHome08")
-    public ResponseEntity<Member> home08() {
-        log.info("home08");
+    @GetMapping("/registerForm")
+    public String registerForm() {
+        log.info("registerForm");
 
-        Member member = new Member();
-
-        return new ResponseEntity<Member>(member, HttpStatus.OK);
-    }
-
-    @ResponseBody
-    @GetMapping("/goHome09")
-    public ResponseEntity<List<Member>> home09() {
-        log.info("home09");
-
-        List<Member> list = new ArrayList<Member>();
-
-        Member member = new Member();
-        list.add(member);
-
-        Member member2 = new Member();
-        list.add(member2);
-
-        return new ResponseEntity<List<Member>>(list, HttpStatus.OK);
-    }
-
-    @ResponseBody
-    @GetMapping("/goHome10")
-    public ResponseEntity<Map<String, Member>> home10() {
-        log.info("home10");
-
-        Map<String, Member> map = new HashMap<String, Member>();
-
-        Member member = new Member();
-        map.put("key1", member);
-
-        Member member2 = new Member();
-        map.put("key2", member2);
-
-        return new ResponseEntity<Map<String, Member>>(map, HttpStatus.OK);
-    }
-
-    @ResponseBody
-    @GetMapping("/goHome1101")
-    public ResponseEntity<byte[]> home1101() throws Exception {
-        log.info("home1101");
-
-        InputStream in = null;
-        ResponseEntity<byte[]> entity = null;
-
-        try {
-            HttpHeaders headers = new HttpHeaders();
-
-            in = new FileInputStream("C:\\TEMP\\sample.png");
-
-            headers.setContentType(MediaType.IMAGE_PNG);
-
-            entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
-        } catch (Exception e) {
-            e.printStackTrace();
-            entity = new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
-        } finally {
-            in.close();
-        }
-        return entity;
-    }
-
-    @ResponseBody
-    @GetMapping(value = "/goHome1102")
-    public ResponseEntity<byte[]> home1102() throws Exception {
-        log.info("home1102");
-
-        String fileName = "aa.zip";
-
-        InputStream in = null;
-        ResponseEntity<byte[]> entity = null;
-
-        try {
-            HttpHeaders headers = new HttpHeaders();
-
-            in = new FileInputStream("C:\\TEMP\\" + fileName);
-
-            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-            headers.add("Content-Disposition", "attachment; filename=\"" +
-                    new String(fileName.getBytes("UTF-8"), "ISO-8859-1") + "\"");
-
-            entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
-        } catch (Exception e) {
-            e.printStackTrace();
-            entity = new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
-        } finally {
-            in.close();
-        }
-        return entity;
+        return "registerForm";
     }
 
 }
--- SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/MemberController.java
+++ SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/MemberController.java
@@ -1,13 +1,13 @@
 package org.hdcd.devproject.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
 import org.hdcd.devproject.domain.*;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
@@ -18,128 +18,139 @@ import java.util.List;
 @Controller
 public class MemberController {
 
-    @GetMapping("/registerForm")
-    public String registerForm() {
-        log.info("registerForm");
+    @GetMapping("/register/{userId}")
+    public ResponseEntity<String> register01(@PathVariable("userId") String userId) {
+        log.info("register01");
 
-        return "registerForm";
+        log.info("userId = " + userId);
+
+        ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+
+        return entity;
     }
 
-    @PostMapping("/registerFile01")
-    public String registerFile01(MultipartFile picture) throws Exception {
-        log.info("registerFile01");
-
-        log.info("originalName: " + picture.getOriginalFilename());
-        log.info("size: " + picture.getSize());
-        log.info("contentType: " + picture.getContentType());
-
-        return "success";
-    }
-
-    @PostMapping("registerFile02")
-    public String registerFile02(String userId, String password, MultipartFile picture) throws Exception {
-        log.info("registerFile02");
+    @PostMapping("/register/{userId}/{password}")
+    public ResponseEntity<String> register02(@PathVariable("userId") String userId
+            , @PathVariable("password") String password) {
+        log.info("register02");
 
         log.info("userId = " + userId);
         log.info("password = " + password);
 
-        log.info("originalName: " + picture.getOriginalFilename());
-        log.info("size: " + picture.getSize());
-        log.info("contentType: " + picture.getContentType());
+        ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
 
-        return "success";
+        return entity;
     }
 
-    @PostMapping("/registerFile03")
-    public String registerFile03(Member member, MultipartFile picture) throws Exception {
-        log.info("registerFile03");
+    @PostMapping("/register03")
+    public ResponseEntity<String> register03(@RequestBody Member member) {
+        log.info("register03");
 
         log.info("userId = " + member.getUserId());
         log.info("password = " + member.getPassword());
 
-        log.info("originalName : " + picture.getOriginalFilename());
-        log.info("size : " + picture.getSize());
-        log.info("contentType : " + picture.getContentType());
+        ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
 
-        return "success";
+        return entity;
     }
 
-    @PostMapping("/registerFile04")
-    public String registerFile04(FileMember fileMember) throws Exception {
-        log.info("registerFile04");
+    @PostMapping("/register04")
+    public ResponseEntity<String> register04(String userId) {
+        log.info("register04");
 
-        log.info("userId = " + fileMember.getUserId());
-        log.info("password = " + fileMember.getPassword());
+        log.info("userId = " + userId);
 
-        MultipartFile picture = fileMember.getPicture();
+        ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
 
-        log.info("originalName : " + picture.getOriginalFilename());
-        log.info("size : " + picture.getSize());
-        log.info("contentType : " + picture.getContentType());
-
-        return "success";
+        return entity;
     }
 
-    @PostMapping("/registerFile05")
-    public String registerFile05(MultipartFile picture, MultipartFile picture2) throws Exception {
-        log.info("registerFile05");
+    @PostMapping("/register05")
+    public ResponseEntity<String> register05(String userId, String password) {
+        log.info("register05");
 
-        log.info("picture originalName: " + picture.getOriginalFilename());
-        log.info("picture size: " + picture.getSize());
-        log.info("picture contentType: " + picture.getContentType());
+        log.info("userId = " + userId);
+        log.info("password = " + password);
 
-        log.info("picture2 originalName: " + picture2.getOriginalFilename());
-        log.info("picture2 size: " + picture2.getSize());
-        log.info("picture2 contentType: " + picture2.getContentType());
+        ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
 
-        return "success";
+        return entity;
     }
 
-    @PostMapping("/registerFile06")
-    public String registerFile06(List<MultipartFile> pictureList) throws Exception {
-        log.info("registerFile06");
+    @PostMapping("/register/{userId}")
+    public ResponseEntity<String> register06(@PathVariable("userId") String userId, @RequestBody Member member) {
+        log.info("register06");
 
-        log.info("registerFile06 pictureList.size() = " + pictureList.size());
+        log.info("userId = " + userId);
 
-        for (MultipartFile picture : pictureList) {
-            log.info("picture originalName : " + picture.getOriginalFilename());
-            log.info("picture size : " + picture.getSize());
-            log.info("picture contentType : " + picture.getContentType());
+        log.info("member.getUserId() = " + member.getUserId());
+        log.info("member.getPassword() = " + member.getPassword());
+
+        ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+
+        return entity;
+    }
+
+    @PostMapping("/register07")
+    public ResponseEntity<String> register07(@RequestBody List<Member> memberList) {
+        log.info("register07");
+
+        for (Member member : memberList) {
+            log.info("userId = " + member.getUserId());
+            log.info("password = " + member.getPassword());
         }
 
-        return "success";
+        ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+
+        return entity;
     }
 
-    @PostMapping("/registerFile07")
-    public String registerFile07(MultiFileMember multiFileMember) throws Exception {
-        log.info("registerFile07");
+    @PostMapping("/register08")
+    public ResponseEntity<String> register08(@RequestBody Member member) {
+        log.info("register08");
 
-        List<MultipartFile> pictureList = multiFileMember.getPictureList();
+        log.info("userId = " + member.getUserId());
+        log.info("password = " + member.getPassword());
 
-        log.info("registerFile07 pictureList.size() = " + pictureList.size());
+        Address address = member.getAddress();
 
-        for (MultipartFile picture : pictureList) {
-            log.info("picture originalName : " + picture.getOriginalFilename());
-            log.info("picture size : " + picture.getSize());
-            log.info("picture contentType : " + picture.getContentType());
+        if (address != null) {
+            log.info("address.getPostCode() = " + address.getPostCode());
+            log.info("address.getLocation() = " + address.getLocation());
+        } else {
+            log.info("address == null");
         }
 
-        return "success";
+        ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+
+        return entity;
     }
 
-    @PostMapping("/registerFile08")
-    public String registerFile08(MultipartFile[] pictureList) throws Exception {
-        log.info("registerFile08");
+    @PostMapping("/register09")
+    public ResponseEntity<String> register09(@RequestBody Member member) {
+        log.info("register09");
 
-        log.info("registerFile08 pictureList.length = " + pictureList.length);
+        log.info("userId = " + member.getUserId());
+        log.info("password = " + member.getPassword());
 
-        for (MultipartFile picture : pictureList) {
-            log.info("picture originalName: " + picture.getOriginalFilename());
-            log.info("picture size: " + picture.getSize());
-            log.info("picture contentType: " + picture.getContentType());
+        List<Card> cardList = member.getCardList();
+
+        if (cardList != null) {
+            log.info("cardList.size() = " + cardList.size());
+
+            for (int i = 0; i < cardList.size(); i++) {
+                Card card = cardList.get(i);
+                log.info("card.getNo() = " + card.getNo());
+                log.info("card.getValidMonth() = " + card.getValidMonth());
+                card.getValidMonth();
+            }
+        } else {
+            log.info("cardList == null");
         }
 
-        return "success";
+        ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+
+        return entity;
     }
 
 }
