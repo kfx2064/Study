ceea1820b525adfd5f8244a788f7c05912b339ca
thJeongGab <jth454@naver.com>
2021-05-14 15:21:41 +0900

    jpa

--- SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/HomeController.java
+++ SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/HomeController.java
@@ -14,15 +14,8 @@ import java.util.Locale;
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Locale locale, Model model) {
-        log.info("Welcome home! The client locale is " + locale + ".");
-
-        LocalDateTime now = LocalDateTime.now();
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일 (E) a h시 m분 s초");
-        String formattedNow = now.format(formatter);
-
-        model.addAttribute("serverTime", formattedNow);
+    public String home(Model model) {
+        model.addAttribute("msg", "Hello world!");
 
         return "home";
     }
