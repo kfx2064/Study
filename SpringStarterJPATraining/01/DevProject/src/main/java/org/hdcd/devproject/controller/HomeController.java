ca82441838278b94d91c4928c828dfe7dae538d4
thJeongGab <jth454@naver.com>
2021-05-14 17:46:14 +0900

    jpa

--- SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/HomeController.java
+++ SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/HomeController.java
@@ -11,37 +11,16 @@ public class HomeController {
 
     @GetMapping("/home0101")
     public String home0101(Model model) {
-        model.addAttribute("username", "Sebastian");
+        model.addAttribute("msg", "Hello world!");
 
         return "home0101";
     }
 
-    @GetMapping("/home0102")
-    public String home0102(Model model) {
-        model.addAttribute("username", "<b>Sebastian</b>");
-
-        return "home0102";
-    }
-
     @GetMapping("/home0201")
     public String home0201(Model model) {
-        model.addAttribute("username", "<b>Sebastian</b>");
+        model.addAttribute("msg", "Hello world!");
 
         return "home0201";
-    }
-
-    @GetMapping("/home0301")
-    public String home0301(Model model) {
-        model.addAttribute("username", "Sebastian");
-
-        return "home0301";
-    }
-
-    @GetMapping("/home0401")
-    public String home0401(Model model) {
-        model.addAttribute("username", "Sebastian");
-
-        return "home0401";
     }
 
 }
