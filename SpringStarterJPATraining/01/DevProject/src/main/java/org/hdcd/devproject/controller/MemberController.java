9f3b40d1c57622c9583e37b2b166d0a947958837
thJeongGab <jth454@naver.com>
2021-05-14 15:16:59 +0900

    jpa

--- SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/MemberController.java
+++ SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/MemberController.java
@@ -1,19 +1,12 @@
 package org.hdcd.devproject.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.hdcd.devproject.domain.Address;
-import org.hdcd.devproject.domain.Card;
 import org.hdcd.devproject.domain.Member;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.time.LocalDate;
-import java.time.YearMonth;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
@@ -26,44 +19,19 @@ public class MemberController {
         return "registerForm";
     }
 
-    @PostMapping("/register01")
-    public String register01(String userId) {
-        log.info("register01");
+    @PostMapping("/register")
+    public String register(Member member, RedirectAttributes rttr) throws Exception {
+        log.info("register");
 
-        log.info("userId = " + userId);
-
-        return "result01";
+        rttr.addFlashAttribute("msg", "success");
+        return "redirect:/result";
     }
 
-    @PostMapping("/register02")
-    public String register02(@ModelAttribute("userId") String userId) {
-        log.info("register02");
+    @GetMapping("/result")
+    public String result() {
+        log.info("result");
 
-        log.info("userId = " + userId);
-
-        return "result02";
-    }
-
-    @PostMapping("/register03")
-    public String register03(@ModelAttribute("userId") String userId, @ModelAttribute("password") String password) {
-        log.info("register03");
-
-        log.info("userId = " + userId);
-
-        log.info("password = " + password);
-
-        return "result03";
-    }
-
-    @PostMapping("/register04")
-    public String register04(Member member) {
-        log.info("register04");
-
-        log.info("userId = " + member.getUserId());
-
-        log.info("password = " + member.getPassword());
-
-        return "result04";
+        return "result";
     }
 
 }
--- SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/domain/Member.java
+++ SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/domain/Member.java
@@ -1,10 +1,6 @@
 package org.hdcd.devproject.domain;
 
 import lombok.*;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @Getter
 @Setter
