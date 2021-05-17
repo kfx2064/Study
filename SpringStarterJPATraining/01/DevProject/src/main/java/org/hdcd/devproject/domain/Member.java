806289b483f726c937ea687c587aa71f8c3a580d
thJeongGab <jth454@naver.com>
2021-05-17 15:46:49 +0900

    jpa

--- SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/domain/Member.java
+++ SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/domain/Member.java
@@ -3,7 +3,9 @@ package org.hdcd.devproject.domain;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
@@ -17,15 +19,18 @@ public class Member {
     @NotBlank
     private String userId;
 
+    @NotBlank
     private String password;
 
     @NotBlank
     @Size(max = 3)
     private String userName;
 
+    @Email
     private String email;
     private String gender;
 
+    @Past
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateOfBirth;
 
