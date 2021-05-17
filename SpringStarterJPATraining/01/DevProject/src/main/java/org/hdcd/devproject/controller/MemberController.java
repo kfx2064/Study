0eca4e2341854e65685230f607ca554767f626f7
thJeongGab <jth454@naver.com>
2021-05-17 11:45:55 +0900

    jpa

--- SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/MemberController.java
+++ SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/MemberController.java
@@ -1,11 +1,17 @@
 package org.hdcd.devproject.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hdcd.devproject.domain.CodeLabelValue;
 import org.hdcd.devproject.domain.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -15,31 +21,51 @@ public class MemberController {
     public String registerForm01(Model model) {
         log.info("registerForm01");
 
+        Map<String, String> hobbyMap = new HashMap<String, String>();
+        hobbyMap.put("01", "Sports");
+        hobbyMap.put("02", "Music");
+        hobbyMap.put("03", "Movie");
+
+        model.addAttribute("hobbyMap", hobbyMap);
+
         model.addAttribute("member", new Member());
 
-        return "registerForm";
+        return "registerForm01";
     }
 
     @GetMapping("/registerForm02")
     public String registerForm02(Model model) {
         log.info("registerForm02");
 
-        Member member = new Member();
+        List<CodeLabelValue> hobbyCodeList = new ArrayList<CodeLabelValue>();
+        hobbyCodeList.add(new CodeLabelValue("01", "Sports"));
+        hobbyCodeList.add(new CodeLabelValue("02", "Music"));
+        hobbyCodeList.add(new CodeLabelValue("03", "Movie"));
 
-        String introduction = "안녕하세요.\n반갑습니다.";
+        model.addAttribute("hobbyCodeList", hobbyCodeList);
 
-        member.setIntroduction(introduction);
+        model.addAttribute("member", new Member());
 
-        model.addAttribute("member", member);
-
-        return "registerForm";
+        return "registerForm02";
     }
 
     @PostMapping("/register")
-    public String register(Member member) {
+    public String register(Member member, Model model) {
         log.info("register");
 
-        log.info("member.getIntroduction() = " + member.getIntroduction());
+        List<String> hobbyList = member.getHobbyList();
+
+        if (hobbyList != null) {
+            log.info("hobbyList != null = " + hobbyList.size());
+
+            for (int i = 0; i < hobbyList.size(); i++) {
+                log.info("hobbyList(" + i + ") = " + hobbyList.get(i));
+            }
+        } else {
+            log.info("hobbyList == null");
+        }
+
+        model.addAttribute("hobbyList", hobbyList);
 
         return "result";
     }
---
+++ SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/domain/CodeLabelValue.java
@@ -0,0 +1,15 @@
+package org.hdcd.devproject.domain;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class CodeLabelValue {
+
+    private String label;
+    private String value;
+
+}
