d8f52d1c75cf1828fa5c7fa8871c031b5ed7c54a
thJeongGab <jth454@naver.com>
2021-05-17 10:52:32 +0900

    jpa

--- SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/MemberController.java
+++ SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/MemberController.java
@@ -5,17 +5,17 @@ import org.hdcd.devproject.domain.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
 public class MemberController {
 
     @GetMapping("/registerForm01")
-    public String registerForm01() {
+    public String registerForm01(Model model) {
         log.info("registerForm01");
+
+        model.addAttribute("member", new Member());
 
         return "registerForm";
     }
@@ -24,64 +24,9 @@ public class MemberController {
     public String registerForm02(Model model) {
         log.info("registerForm02");
 
-        model.addAttribute("member", new Member());
-
-        return "registerForm";
-    }
-
-    @GetMapping("/registerForm03")
-    public String registerForm03(Model model) {
-        log.info("registerForm03");
-
-        model.addAttribute("user", new Member());
-
-        return "registerForm";
-    }
-
-    @GetMapping("/registerForm04")
-    public String registerForm04(Model model) {
-        log.info("registerForm04");
-
-        model.addAttribute("user", new Member());
-
-        return "registerForm2";
-    }
-
-    @GetMapping("/registerForm05")
-    public String registerForm05(Member member) {
-        log.info("registerForm05");
-
-        return "registerForm";
-    }
-
-    @GetMapping("/registerForm06")
-    public String registerForm06(Member user) {
-        log.info("registerForm06");
-
-        return "registerForm";
-    }
-
-    @GetMapping("/registerForm07")
-    public String registerForm07(@ModelAttribute("user") Member member) {
-        log.info("registerForm07");
-
-        return "registerForm";
-    }
-
-    @GetMapping("/registerForm08")
-    public String registerForm08(@ModelAttribute("user") Member member) {
-        log.info("registerForm08");
-
-        return "registerForm2";
-    }
-
-    @GetMapping("/registerForm09")
-    public String registerForm09(Model model) {
-        log.info("registerForm09");
-
         Member member = new Member();
 
-        member.setUserId("hongkd");
+        member.setEmail("aaa@ccc.com");
         member.setUserName("홍길동");
 
         model.addAttribute("member", member);
@@ -95,6 +40,7 @@ public class MemberController {
 
         log.info("member.getUserId() = " + member.getUserId());
         log.info("member.getUserName() = " + member.getUserName());
+        log.info("member.getEmail() = " + member.getEmail());
 
         return "result";
     }
--- SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/domain/Member.java
+++ SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/domain/Member.java
@@ -1,6 +1,7 @@
 package org.hdcd.devproject.domain;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,7 +14,30 @@ import java.util.List;
 public class Member {
 
     private String userId;
+    private String password;
 
     private String userName;
+
+    private String email;
+    private String gender;
+    private String hobby;
+    private String[] hobbyArray;
+    private List<String> hobbyList;
+    private boolean foreigner;
+    private String developer;
+    private String nationality;
+
+    private Address address;
+
+    private List<Card> cardList;
+
+    private String cars;
+    private String[] carArray;
+    private List<String> carList;
+
+    private String introduction;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dateOfBirth;
 
 }
