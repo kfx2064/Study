476eac982999f82cd06e6bc8d716f7141e58d352
thJeongGab <jth454@naver.com>
2021-05-17 13:23:14 +0900

    jpa

--- SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/MemberController.java
+++ SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/MemberController.java
@@ -1,17 +1,11 @@
 package org.hdcd.devproject.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.hdcd.devproject.domain.CodeLabelValue;
 import org.hdcd.devproject.domain.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @Controller
@@ -21,33 +15,22 @@ public class MemberController {
     public String registerForm01(Model model) {
         log.info("registerForm01");
 
-        Map<String, String> genderCodeMap = new HashMap<String, String>();
-        genderCodeMap.put("01", "Male");
-        genderCodeMap.put("02", "Female");
-        genderCodeMap.put("03", "Other");
-
-        model.addAttribute("genderCodeMap", genderCodeMap);
-
         model.addAttribute("member", new Member());
 
-        return "registerForm01";
+        return "registerForm";
     }
 
     @GetMapping("/registerForm02")
     public String registerForm02(Model model) {
         log.info("registerForm02");
 
-        List<CodeLabelValue> genderCodeList = new ArrayList<CodeLabelValue>();
+        Member member = new Member();
 
-        genderCodeList.add(new CodeLabelValue("01", "Male"));
-        genderCodeList.add(new CodeLabelValue("02", "Female"));
-        genderCodeList.add(new CodeLabelValue("03", "Other"));
+        member.setGender("female");
 
-        model.addAttribute("genderCodeList", genderCodeList);
+        model.addAttribute("member", member);
 
-        model.addAttribute("member", new Member());
-
-        return "registerForm02";
+        return "registerForm";
     }
 
     @PostMapping("/register")
@@ -56,7 +39,7 @@ public class MemberController {
 
         log.info("member.getGender() = " + member.getGender());
 
-        model.addAttribute("gender", member.getGender());
+        model.addAttribute("member", member);
 
         return "result";
     }
