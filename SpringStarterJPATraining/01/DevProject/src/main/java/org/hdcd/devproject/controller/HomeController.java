d3657477e9114852744b834a9dcd59d8d5032819
thJeongGab <jth454@naver.com>
2021-05-17 16:56:07 +0900

    jpa

--- SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/HomeController.java
+++ SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/HomeController.java
@@ -1,6 +1,8 @@
 package org.hdcd.devproject.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +13,11 @@ import java.util.Date;
 import java.util.Locale;
 
 @Slf4j
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
+
+    private final MessageSource messageSource;
 
     @GetMapping("/")
     public String home(Locale locale, Model model) {
@@ -24,6 +29,17 @@ public class HomeController {
         String formattedNow = now.format(formatter);
 
         model.addAttribute("serverTime", formattedNow);
+
+        return "home";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        String[] args = {"홍길동"};
+
+        String message = messageSource.getMessage("welcome.message", args, Locale.KOREAN);
+
+        log.info("Welcome message : " + message);
 
         return "home";
     }
