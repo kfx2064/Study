ce482c644df1c7b1412f8105d8962bf3c98b2d9b
thJeongGab <jth454@naver.com>
2021-05-17 10:22:40 +0900

    jpa

--- SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/HomeController.java
+++ SpringStarterJPATraining/01/DevProject/src/main/java/org/hdcd/devproject/controller/HomeController.java
@@ -5,22 +5,47 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Slf4j
 @Controller
 public class HomeController {
 
     @GetMapping("/home0101")
     public String home0101(Model model) {
-        model.addAttribute("msg", "Hello world!");
+        int coin = 1000;
+
+        model.addAttribute("coin", coin);
 
         return "home0101";
     }
 
     @GetMapping("/home0201")
     public String home0201(Model model) {
-        model.addAttribute("msg", "Hello world!");
+        Date date = new Date();
+
+        model.addAttribute("now", date);
 
         return "home0201";
+    }
+
+    @GetMapping("/home0301")
+    public String home0301(Model model) {
+        String str = "Hello, World!";
+
+        model.addAttribute("str", str);
+
+        return "home0301";
+    }
+
+    @GetMapping("/home0401")
+    public String home0401(Model model) {
+        LocalDateTime now = LocalDateTime.now();
+
+        model.addAttribute("now", now);
+
+        return "home0401";
     }
 
 }
