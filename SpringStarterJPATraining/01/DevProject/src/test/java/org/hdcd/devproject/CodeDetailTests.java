2cbcd7c92af37df899b7b0488fd89413cec2352d
thJeongGab <jth454@naver.com>
2021-05-27 10:01:59 +0900

    jpa

---
+++ SpringStarterJPATraining/01/DevProject/src/test/java/org/hdcd/devproject/CodeDetailTests.java
@@ -0,0 +1,72 @@
+package org.hdcd.devproject;
+
+import org.hdcd.devproject.domain.CodeDetail;
+import org.hdcd.devproject.repository.CodeDetailRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+public class CodeDetailTests {
+
+    @Autowired
+    CodeDetailRepository codeDetailRepository;
+
+    @Test
+    public void testRegister() {
+        CodeDetail codeDetail = new CodeDetail();
+        codeDetail.setGroupCode("A01");
+        codeDetail.setCodeValue("00");
+        codeDetail.setCodeName("Developer");
+
+        codeDetailRepository.save(codeDetail);
+
+        codeDetail = new CodeDetail();
+
+        codeDetail.setGroupCode("A01");
+        codeDetail.setCodeValue("01");
+        codeDetail.setCodeName("Designer");
+
+        codeDetailRepository.save(codeDetail);
+    }
+
+    @Test
+    public void testList() {
+        Iterable<CodeDetail> codeDetailList = codeDetailRepository.findAll();
+
+        for (CodeDetail codeDetail : codeDetailList) {
+            System.out.println(codeDetail);
+        }
+    }
+
+    @Test
+    public void testRead() {
+        Optional<CodeDetail> codeDetailOptional = codeDetailRepository.findById(1L);
+
+        if (codeDetailOptional.isPresent()) {
+            CodeDetail codeDetail = codeDetailOptional.get();
+
+            System.out.println(codeDetail);
+        }
+    }
+
+    @Test
+    public void testModify() {
+        Optional<CodeDetail> codeDetailOptional = codeDetailRepository.findById(1L);
+
+        if (codeDetailOptional.isPresent()) {
+            CodeDetail codeDetail = codeDetailOptional.get();
+            codeDetail.setCodeName("Architect");
+
+            codeDetailRepository.save(codeDetail);
+        }
+    }
+
+    @Test
+    public void testRemove() {
+        codeDetailRepository.deleteById(1L);
+    }
+
+}
