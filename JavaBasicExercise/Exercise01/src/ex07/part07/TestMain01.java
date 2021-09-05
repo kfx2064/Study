d951420de861d54fff4f991c95d40c7fa40648df
kfx2064 <jth454@naver.com>
2021-09-05 19:31:57 +0900

    자바 연습 중

---
+++ JavaBasicExercise/Exercise01/src/ex07/part07/TestMain01.java
@@ -0,0 +1,28 @@
+package ex07.part07;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class TestMain01 {
+
+    public static void main(String[] args) {
+
+        List<String> strings = Arrays.asList("a", "", "j", "k", "j");
+        List<Integer> numbers = Arrays.asList(3, 7, 6, 8, 2, 4, 1);
+
+        Stream<String> skipStream = strings.stream().skip(2);
+        System.out.println("skip");
+        skipStream.forEach(item -> System.out.print(item + " "));
+        System.out.println();
+
+        Stream<String> stream1 = strings.stream().distinct().filter(item -> !item.isEmpty());
+        Stream<String> stream2 = Arrays.asList("m", "j").stream();
+        Stream<String> concat = Stream.concat(stream1, stream2);
+        System.out.println("concat");
+        concat.forEach(item -> System.out.print(item + " "));
+
+    }
+
+}
