4b56e903679a6ade1cffc37c578c63a9b5087963
kfx2064 <jth454@naver.com>
2021-09-05 19:10:06 +0900

    자바 연습 중

---
+++ JavaBasicExercise/Exercise01/src/ex07/part05/TestMain01.java
@@ -0,0 +1,31 @@
+package ex07.part05;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class TestMain01 {
+
+    public static void main(String[] args) {
+
+        List<String> strings = Arrays.asList("a", "", "j", "k", "j");
+        List<Integer> numbers = Arrays.asList(3, 7, 6, 8, 2, 4, 1);
+
+        Stream<String> filter = strings.stream().filter(s -> !s.isEmpty());
+        System.out.println("filter()");
+        filter.forEach(item -> System.out.print(item + " "));
+        System.out.println();
+
+        Stream<Integer> filter2 = numbers.stream().filter(i -> i > 2);
+        System.out.println("filter");
+        filter2.forEach(item -> System.out.print(item + " "));
+        System.out.println();
+
+        Stream<String> distinct = strings.stream().distinct().filter(s -> !s.isEmpty());
+        System.out.println("distinct");
+        distinct.forEach(item -> System.out.print(item + " "));
+        System.out.println();
+
+    }
+
+}
