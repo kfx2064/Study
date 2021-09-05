ca218746089018d8fe7348b2629c74920b190fcd
kfx2064 <jth454@naver.com>
2021-09-05 13:10:59 +0900

    자바 연습 중

---
+++ JavaBasicExercise/Exercise01/src/ex06/part05/TM02.java
@@ -0,0 +1,45 @@
+package ex06.part05;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class TM02 {
+
+    public static void main(String[] args) {
+
+        List<String> names1 = new ArrayList<>();
+        names1.add("b");
+        names1.add("c");
+        names1.add("a");
+
+        List<String> names2 = new ArrayList<>();
+        names2.add("b");
+        names2.add("c");
+        names2.add("a");
+
+        TM02 test = new TM02();
+
+        test.sort1(names1);
+        test.sort2(names2);
+
+        System.out.println(names1);
+        System.out.println(names2);
+
+    }
+
+    private void sort1(List<String> names) {
+        Collections.sort(names, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return s1.compareTo(s2);
+            }
+        });
+    }
+
+    private void sort2(List<String> names) {
+        Collections.sort(names, (s1, s2) -> s1.compareTo(s2));
+    }
+
+}
