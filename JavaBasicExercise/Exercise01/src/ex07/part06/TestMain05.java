5b4f0bd2390594c07d3e6136bb4697b500b4a938
kfx2064 <jth454@naver.com>
2021-09-05 19:24:22 +0900

    자바 연습 중

---
+++ JavaBasicExercise/Exercise01/src/ex07/part06/TestMain05.java
@@ -0,0 +1,31 @@
+package ex07.part06;
+
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class TestMain05 {
+
+    public static void main(String[] args) {
+
+        List<String> strings = Arrays.asList("a", "", "j", "k", "j");
+        List<Integer> numbers = Arrays.asList(3, 7, 6, 8, 2, 4, 1);
+
+        Stream<Integer> sorted = numbers.stream().sorted();
+        System.out.println("sorted");
+        sorted.forEach(item -> System.out.print(item + " "));
+        System.out.println();
+
+        Stream<String> sorted2 = strings.stream().distinct().sorted((s1, s2) -> s1.compareTo(s2));
+        System.out.println("sorted");
+        sorted2.forEach(item -> System.out.print(item + " "));
+        System.out.println();
+
+        Stream<String> limit = strings.stream().limit(3);
+        System.out.println("limit");
+        limit.forEach(item -> System.out.print(item + " "));
+
+    }
+
+}
