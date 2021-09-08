dbb4f6c12a82a4db2ec7841f5eb76d1314b7f488
jth454@naver.com <dksdid@8282>
2021-09-08 13:31:44 +0900

    자바 연습 중.

---
+++ JavaBasicExercise/Exercise01/src/ex09/part01/TypeA.java
@@ -0,0 +1,23 @@
+package ex09.part01;
+
+public class TypeA {
+
+    private String name = "name 속성";
+
+    public TypeA() {}
+    public TypeA(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public static void method1() {
+        System.out.println("TypeA.method1() - static method1 함수 호출");
+    }
+
+}
---
+++ JavaBasicExercise/Exercise01/src/ex09/part02/TestMain2.java
@@ -0,0 +1,34 @@
+package ex09.part02;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class TestMain2 {
+
+    public static void main(String[] args) throws Exception {
+
+        Class forName = Class.forName("ex09.part02.TypeA");
+        Object a1 = forName.newInstance();
+
+        Method method1 = forName.getDeclaredMethod("getName");
+        Object name1 = method1.invoke(a1);
+        System.out.println("a1 name : " + name1);
+
+        Method method2 = forName.getDeclaredMethod("setName", String.class);
+        method2.invoke(a1, "name2");
+
+        Object name2 = method1.invoke(a1);
+        System.out.println("a1 name : " + name2);
+
+        Constructor constructor = forName.getDeclaredConstructor(String.class);
+        Object a2 = constructor.newInstance("name3");
+
+        Object name3 = method1.invoke(a2);
+        System.out.println("a2 name : " + name3);
+
+        Method method3 = forName.getDeclaredMethod("method1");
+        method3.invoke(null);
+
+    }
+
+}
