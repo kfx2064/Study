03f5f84481ac1f0b354b140d869f15352db08920
jth454@naver.com <dksdid@8282>
2021-09-08 10:35:24 +0900

    자바 연습 중.

---
+++ JavaBasicExercise/Exercise01/src/ex08/part01/TestMain03.java
@@ -0,0 +1,31 @@
+package ex08.part01;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class TestMain03 {
+
+    public static void main(String[] args) {
+
+        List<String> strings = Arrays.asList("aaa", "bbb", "", "CCC", "bbb");
+
+        List<Integer> numbers = Arrays.asList(3, 7, 6, 8, 2, 4, 1);
+
+        String[] array = strings.stream().toArray(String[]::new);
+        System.out.println("toArray : " + Arrays.toString(array));
+
+        Integer reduce1 = numbers.stream().reduce(0, Integer::sum);
+        System.out.println("reduce1 : " + reduce1);
+
+        Set<String> collect = strings.stream().collect(Collectors.toSet());
+        System.out.println("collect : " + collect);
+
+        List<String> collect2 = Stream.of("a", "b", "c").collect(Collectors.toList());
+        System.out.println("collect2 : " + collect2);
+
+    }
+
+}
