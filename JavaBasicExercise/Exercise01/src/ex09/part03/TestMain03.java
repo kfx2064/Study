7f77851a536a9776b49e8e0203674572fa235be9
jth454@naver.com <dksdid@8282>
2021-09-10 11:31:26 +0900

    자바 연습 중.

---
+++ JavaBasicExercise/Exercise01/src/ex09/part03/TestMain03.java
@@ -0,0 +1,53 @@
+package ex09.part03;
+
+import com.sun.security.jgss.GSSUtil;
+
+public class TestMain03 {
+
+    public static void main(String[] args) {
+
+        TestMain03 test = new TestMain03();
+
+        Math add = (x, y) -> x + y;
+        Math substract = (int x, int y) -> x - y;
+        Math multiply = (x, y) -> {
+            return x * y;
+        };
+        Math divide = (int x, int y) -> x / y;
+
+        System.out.println("20 + 5 = " + test.operation(20, 5, add));
+        System.out.println("20 - 5 = " + test.operation(20, 5, substract));
+        System.out.println("20 * 5 = " + test.operation(20, 5, multiply));
+        System.out.println("20 / 5 = " + test.operation(20, 5, divide));
+
+        Echo msg1 = (msg) -> {
+            System.out.println("Hello! " + msg);
+        };
+
+        Echo msg2 = msg -> System.out.println("Hello! " + msg);
+
+        msg1.echo("a");
+        msg2.echo("b");
+
+        Process process = () -> System.out.println("no parameter!");
+        process.echo();
+
+    }
+
+    private int operation(int x, int y, Math math) {
+        return math.operation(x, y);
+    }
+
+    interface Math {
+        int operation(int x, int y);
+    }
+
+    interface Echo {
+        void echo(String msg);
+    }
+
+    interface Process {
+        void echo();
+    }
+
+}
