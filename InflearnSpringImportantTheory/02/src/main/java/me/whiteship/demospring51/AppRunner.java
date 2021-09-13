f7b48d581e28a0d75636e13fd3dfd6384c70bc2b
jth454@naver.com <dksdid@8282>
2021-09-13 14:34:11 +0900

    자바 연습 중.

--- InflearnSpringImportantTheory/02/src/main/java/me/whiteship/demospring51/AppRunner.java
+++ InflearnSpringImportantTheory/02/src/main/java/me/whiteship/demospring51/AppRunner.java
@@ -7,21 +7,35 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ResourceLoader resourceLoader;
+    Validator validator;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Resource resource = resourceLoader.getResource("classpath:test.txt");
-        System.out.println(resource.exists());
-        System.out.println(resource.getDescription());
-        System.out.println(Files.readString(Path.of(resource.getURI())));
+        System.out.println(validator.getClass());
+
+        Event event = new Event();
+        Errors errors = new BeanPropertyBindingResult(event, "event");
+
+        validator.validate(event, errors);
+
+        System.out.println(errors.hasErrors());
+
+        errors.getAllErrors().forEach(e -> {
+            System.out.println("=== error code ===");
+            Arrays.stream(e.getCodes()).forEach(System.out::println);
+            System.out.println(e.getDefaultMessage());
+        });
     }
 }
