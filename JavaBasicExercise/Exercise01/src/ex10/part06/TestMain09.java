a87bbb13724047b23f62bbc8f300938afaf63e66
jth454@naver.com <dksdid@8282>
2021-09-13 15:51:02 +0900

    자바 연습 중.

---
+++ JavaBasicExercise/Exercise01/src/ex10/part06/TestMain09.java
@@ -0,0 +1,28 @@
+package ex10.part06;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class TestMain09 {
+
+    public static void main(String[] args) {
+
+        String[] array = {"a", "b", "c"};
+        List<String> list = Arrays.asList(array);
+
+        IntStream range = IntStream.range(5, 9);
+        range.forEach(item -> System.out.print(item + " "));
+        System.out.println();
+
+        IntStream rangeClosed = IntStream.rangeClosed(5, 9);
+        rangeClosed.forEach(item -> System.out.print(item + " "));
+        System.out.println();
+
+        IntStream ints = new Random().ints();
+        ints.limit(3).forEach(item -> System.out.print(item + " "));
+
+    }
+
+}
