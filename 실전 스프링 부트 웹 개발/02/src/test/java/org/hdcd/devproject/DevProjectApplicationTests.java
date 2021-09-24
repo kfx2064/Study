d40b591dc9a0c303d26c8820e008336fddfbb7a9
jth454@naver.com <dksdid@8282>
2021-09-24 13:36:27 +0900

    first training.

--- 실전 스프링 부트 웹 개발/02/src/test/java/org/hdcd/devproject/DevProjectApplicationTests.java
+++ 실전 스프링 부트 웹 개발/02/src/test/java/org/hdcd/devproject/DevProjectApplicationTests.java
@@ -1,12 +1,10 @@
 package org.hdcd.devproject;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DevProjectApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
