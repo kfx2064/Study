4abccd6a74e76647e66ba668684f2dcbfec5979b
jth454@naver.com <dksdid@8282>
2021-09-24 13:21:13 +0900

    spring boot sample training.

---
+++ 실전 스프링 부트 웹 개발/02/src/main/java/org/hdcd/devproject/DevProjectApplication.java
@@ -0,0 +1,13 @@
+package org.hdcd.devproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevProjectApplication.class, args);
+    }
+
+}
