26aa3f986fe132206f373034e47640c99e87219d
jth454@naver.com <dksdid@8282>
2021-09-24 17:40:03 +0900

    mapping training.

--- 실전 스프링 부트 웹 개발/02/src/main/java/org/hdcd/devproject/controller/HomeController.java
+++ 실전 스프링 부트 웹 개발/02/src/main/java/org/hdcd/devproject/controller/HomeController.java
@@ -1,9 +1,12 @@
 package org.hdcd.devproject.controller;
 
+import org.apache.commons.io.IOUtils;
 import org.hdcd.devproject.domain.Member;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.xml.validation.Validator;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
 import java.util.*;
 
@@ -181,6 +187,60 @@ public class HomeController {
         map.put("key2", member2);
 
         return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/goHome1101", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> home1101() throws Exception {
+        logger.info("home1101");
+
+        InputStream in = null;
+        ResponseEntity<byte[]> entity = null;
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+
+            in = new FileInputStream("C:\\TEMP\\sample.png");
+
+            headers.setContentType(MediaType.IMAGE_PNG);
+
+            entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } finally {
+            in.close();
+        }
+        return entity;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/goHome1102", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> home1102() throws Exception {
+        logger.info("home1102");
+
+        String fileName = "aa.zip";
+
+        InputStream in = null;
+        ResponseEntity<byte[]> entity = null;
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+
+            in = new FileInputStream("C:\\TEMP\\" + fileName);
+
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            headers.add("Content-Disposition", "attachment; filename=\"" +
+                    new String(fileName.getBytes("UTF-8"), "ISO-8859-1") + "\"");
+
+            entity = new ResponseEntity<>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } finally {
+            in.close();
+        }
+        return entity;
     }
 
 }
