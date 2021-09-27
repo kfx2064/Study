b2cf906cd802209563d6a5e19b8d28d23844528e
jth454@naver.com <dksdid@8282>
2021-09-27 14:15:30 +0900

    request training.

--- 실전 스프링 부트 웹 개발/02/src/main/java/org/hdcd/devproject/domain/Member.java
+++ 실전 스프링 부트 웹 개발/02/src/main/java/org/hdcd/devproject/domain/Member.java
@@ -1,12 +1,16 @@
 package org.hdcd.devproject.domain;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Member implements Serializable {
 
     private static final long serialVersionUID = 5691454397829345004L;
-    private String userId;
-    private String password;
+    private String userId = "hongkd";
+    private String password = "1234";
+
+    private Address address;
+    private List<Card> cardList;
 
     public String getUserId() {
         return userId;
@@ -22,5 +26,21 @@ public class Member implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<Card> getCardList() {
+        return cardList;
+    }
+
+    public void setCardList(List<Card> cardList) {
+        this.cardList = cardList;
     }
 }
