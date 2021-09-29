662b97aecef39b10e35ae5e086cba25286ccd45e
jth454@naver.com <dksdid@8282>
2021-09-29 12:39:54 +0900

    request training.

--- 실전 스프링 부트 웹 개발/02/src/main/java/org/hdcd/devproject/controller/MemberController.java
+++ 실전 스프링 부트 웹 개발/02/src/main/java/org/hdcd/devproject/controller/MemberController.java
@@ -1,242 +1,56 @@
 package org.hdcd.devproject.controller;
 
-import org.hdcd.devproject.domain.*;
+import org.hdcd.devproject.domain.Member;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
 
 @Controller
 public class MemberController {
 
     private final static Logger logger = LoggerFactory.getLogger(MemberController.class);
 
-    @RequestMapping(value = "/read01", method = RequestMethod.GET)
-    public String read01(Model model) {
-        logger.info("read01");
-
-        model.addAttribute("userId", "hongkd");
-        model.addAttribute("password", "1234");
-        model.addAttribute("email", "aaa@ccc.com");
-        model.addAttribute("userName", "홍길동");
-        model.addAttribute("birthDay", "1989-09-07");
-
-        return "read01";
+    @RequestMapping(value = "/registerForm", method = RequestMethod.GET)
+    public String registerForm() {
+        logger.info("registerForm");
+        return "registerForm";
     }
 
-    @RequestMapping(value = "/read02", method = RequestMethod.GET)
-    public String read02(Model model) {
-        logger.info("read02");
-
-        Member member = new Member();
-
-        member.setUserId("hongkd");
-        member.setPassword("1234");
-        member.setEmail("aaa@ccc.com");
-        member.setUserName("홍길동");
-        member.setBirthDay("1989-09-07");
-
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, 1988);
-        cal.set(Calendar.MONTH, 10);
-        cal.set(Calendar.DAY_OF_MONTH, 7);
-
-        member.setDateOfBirth(cal.getTime());
-
-        model.addAttribute(member);
-
-        return "read02";
+    @RequestMapping(value = "/register01", method = RequestMethod.POST)
+    public String register01(String userId) {
+        logger.info("register01");
+        logger.info("userId = " + userId);
+        return "result01";
     }
 
-    @RequestMapping(value = "/read03", method = RequestMethod.GET)
-    public String read03(Model model) {
-        logger.info("read03");
+    @RequestMapping(value = "/register02", method = RequestMethod.POST)
+    public String register02(@ModelAttribute("userId") String userId) {
+        logger.info("register02");
+        logger.info("userId = " + userId);
 
-        Member member = new Member();
-
-        member.setUserId("hongkd");
-        member.setPassword("1234");
-        member.setEmail("aaa@ccc.com");
-        member.setUserName("홍길동");
-        member.setBirthDay("1989-09-07");
-
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, 1988);
-        cal.set(Calendar.MONTH, 10);
-        cal.set(Calendar.DAY_OF_MONTH, 7);
-
-        member.setDateOfBirth(cal.getTime());
-
-        model.addAttribute("user", member);
-
-        return "read03";
+        return "result02";
     }
 
-    @RequestMapping(value = "/read04", method = RequestMethod.GET)
-    public String read04(Model model) {
-        logger.info("read04");
+    @RequestMapping(value = "/register03", method = RequestMethod.POST)
+    public String register03(@ModelAttribute("userId") String userId
+            , @ModelAttribute("password") String password) {
+        logger.info("register03");
+        logger.info("userId = " + userId);
+        logger.info("password = " + password);
 
-        String[] carArray = {"saab", "audi"};
-
-        List<String> carList = new ArrayList<>();
-        carList.add("saab");
-        carList.add("audi");
-
-        String[] hobbyArray = {"Music", "Movie"};
-
-        List<String> hobbyList = new ArrayList<>();
-        hobbyList.add("Music");
-        hobbyList.add("Movie");
-
-        model.addAttribute("carArray", carArray);
-        model.addAttribute("carList", carList);
-
-        model.addAttribute("hobbyArray", hobbyArray);
-        model.addAttribute("hobbyList", hobbyList);
-
-        return "read04";
+        return "result03";
     }
 
-    @RequestMapping(value = "/read05", method = RequestMethod.GET)
-    public String read05(Model model) {
-        logger.info("read05");
+    @RequestMapping(value = "/register04", method = RequestMethod.POST)
+    public String register04(Member member) {
+        logger.info("register04");
+        logger.info("userId = " + member.getUserId());
+        logger.info("password = " + member.getPassword());
 
-        Member member = new Member();
-
-        Address address = new Address();
-        address.setPostCode("080908");
-        address.setLocation("seoul");
-
-        member.setAddress(address);
-
-        List<Card> cardList = new ArrayList<>();
-
-        Card card1 = new Card();
-        card1.setNo("123456");
-
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, 2020);
-        cal.set(Calendar.MONTH, 9);
-        cal.set(Calendar.DAY_OF_MONTH, 8);
-
-        card1.setValidMonth(cal.getTime());
-
-        cardList.add(card1);
-
-        Card card2 = new Card();
-        card2.setNo("456786");
-
-        cal.set(Calendar.YEAR, 2022);
-        cal.set(Calendar.MONTH, 11);
-        cal.set(Calendar.DAY_OF_MONTH, 7);
-
-        card2.setValidMonth(cal.getTime());
-
-        cardList.add(card2);
-
-        member.setCardList(cardList);
-
-        model.addAttribute("user", member);
-
-        return "read05";
-    }
-
-    @RequestMapping(value = "/read06", method = RequestMethod.GET)
-    public String read06(Model model) {
-        logger.info("read06");
-
-        Member member = new Member();
-
-        member.setUserId("hongkd");
-        member.setPassword("1234");
-        member.setEmail("aaa@ccc.com");
-        member.setUserName("홍길동");
-        member.setBirthDay("1989-09-07");
-        member.setGender("female");
-        member.setDeveloper("Y");
-        member.setForeigner(true);
-
-        member.setNationality("Australia");
-
-        member.setCars("saab");
-
-        String[] carArray = {"saab", "audi"};
-
-        member.setCarArray(carArray);
-
-        List<String> carList = new ArrayList<>();
-        carList.add("saab");
-        carList.add("audi");
-
-        member.setCarList(carList);
-
-        member.setHobby("Movie");
-
-        String[] hobbyArray = {"Music", "Movie"};
-
-        member.setHobbyArray(hobbyArray);
-
-        List<String> hobbyList = new ArrayList<>();
-        hobbyList.add("Music");
-        hobbyList.add("Movie");
-
-        member.setHobbyList(hobbyList);
-
-        Address address = new Address();
-        address.setPostCode("080908");
-        address.setLocation("seoul");
-
-        member.setAddress(address);
-
-        List<Card> cardList = new ArrayList<>();
-
-        Card card1 = new Card();
-        card1.setNo("123456");
-
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, 2020);
-        cal.set(Calendar.MONTH, 9);
-        cal.set(Calendar.DAY_OF_MONTH, 8);
-
-        card1.setValidMonth(cal.getTime());
-
-        cardList.add(card1);
-
-        Card card2 = new Card();
-        card2.setNo("456786");
-
-        cal.set(Calendar.YEAR, 2022);
-        cal.set(Calendar.MONTH, 11);
-        cal.set(Calendar.DAY_OF_MONTH, 7);
-
-        card2.setValidMonth(cal.getTime());
-
-        cardList.add(card2);
-
-        member.setCardList(cardList);
-
-        cal.set(Calendar.YEAR, 1988);
-        cal.set(Calendar.MONTH, 10);
-        cal.set(Calendar.DAY_OF_MONTH, 7);
-
-        member.setDateOfBirth(cal.getTime());
-
-        String introduction = "안녕하세요.\n반갑습니다.";
-
-        member.setIntroduction(introduction);
-
-        model.addAttribute("user", member);
-
-        return "read06";
+        return "result04";
     }
 
 }
