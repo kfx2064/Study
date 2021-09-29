0f45d2e0f0803e0319cd81f0643037fd8bee7472
jth454@naver.com <dksdid@8282>
2021-09-29 12:48:02 +0900

    타임리프.

--- 실전 스프링 부트 웹 개발/02/src/main/java/org/hdcd/devproject/controller/MemberController.java
+++ 실전 스프링 부트 웹 개발/02/src/main/java/org/hdcd/devproject/controller/MemberController.java
@@ -4,6 +4,7 @@ import org.hdcd.devproject.domain.Member;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,22 +15,10 @@ public class MemberController {
 
     private final static Logger logger = LoggerFactory.getLogger(MemberController.class);
 
-    @RequestMapping(value = "/registerForm", method = RequestMethod.GET)
-    public String requestForm() {
-        logger.info("registerForm");
-        return "registerForm";
-    }
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Model model) {
+        model.addAttribute("msg", "Hello world!");
 
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String register(Member member, RedirectAttributes rttr) throws Exception {
-        logger.info("register");
-        rttr.addFlashAttribute("msg", "success");
-        return "redirect:/result";
-    }
-
-    @RequestMapping(value = "/result", method = RequestMethod.GET)
-    public String result() {
-        logger.info("result");
-        return "result";
+        return "home";
     }
 }
