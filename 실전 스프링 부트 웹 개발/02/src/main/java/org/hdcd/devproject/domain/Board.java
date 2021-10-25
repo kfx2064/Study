10de31e245d3debf1e1c541134b7359b1a98566e
kfx2064 <jth454@naver.com>
2021-10-25 20:35:01 +0900

    thymeleaf.

--- 실전 스프링 부트 웹 개발/02/src/main/java/org/hdcd/devproject/domain/Board.java
+++ 실전 스프링 부트 웹 개발/02/src/main/java/org/hdcd/devproject/domain/Board.java
@@ -1,20 +1,37 @@
 package org.hdcd.devproject.domain;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Date;
 
-public class Board {
+@Entity
+@Table(name = "board")
+public class Board implements Serializable {
 
-    private int boardNo;
+    private static final long serialVersionUID = 2532755891791625050L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "board_no")
+    private Integer boardNo;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "content")
     private String content;
+
+    @Column(name = "writer")
     private String writer;
+
+    @Column(name = "reg_date")
     private Date regDate;
 
-    public int getBoardNo() {
+    public Integer getBoardNo() {
         return boardNo;
     }
 
-    public void setBoardNo(int boardNo) {
+    public void setBoardNo(Integer boardNo) {
         this.boardNo = boardNo;
     }
 
@@ -48,5 +65,16 @@ public class Board {
 
     public void setRegDate(Date regDate) {
         this.regDate = regDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Board{" +
+                "boardNo=" + boardNo +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", writer='" + writer + '\'' +
+                ", regDate=" + regDate +
+                '}';
     }
 }
