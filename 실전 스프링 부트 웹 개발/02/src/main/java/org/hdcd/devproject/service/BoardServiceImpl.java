850d7cfa5269689d56ed06523987d09498c6f768
kfx2064 <jth454@naver.com>
2021-10-27 20:27:50 +0900

    skip jpa.

--- 실전 스프링 부트 웹 개발/02/src/main/java/org/hdcd/devproject/service/BoardServiceImpl.java
+++ 실전 스프링 부트 웹 개발/02/src/main/java/org/hdcd/devproject/service/BoardServiceImpl.java
@@ -2,39 +2,15 @@ package org.hdcd.devproject.service;
 
 import org.hdcd.devproject.dao.BoardDAO;
 import org.hdcd.devproject.domain.Board;
+import org.hdcd.devproject.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BoardServiceImpl implements BoardService {
 
-    @Autowired
-    private BoardDAO dao;
-
-    @Override
-    public void register(Board board) throws Exception {
-        dao.create(board);
-    }
-
-    @Override
-    public Board read(Integer boardNo) throws Exception {
-        return dao.read(boardNo);
-    }
-
-    @Override
-    public void modify(Board board) throws Exception {
-        dao.update(board);
-    }
-
-    @Override
-    public void remove(Integer boardNo) throws Exception {
-        dao.delete(boardNo);
-    }
-
-    @Override
-    public List<Board> list() throws Exception {
-        return dao.list();
-    }
 }
