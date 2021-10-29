5798c0854de67267e12b688ac54277652c3619cc
jth454@naver.com <dksdid@8282>
2021-10-29 10:10:24 +0900

    python training.

---
+++ python/01/python01/first/Hello.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2021. 10. 29.
+
+@author: USER
+'''
+print("Hello world.");
+num1 = 100;
+num2 = 50;
+result = num1 + num2;
+print(result);
+result1 = num1 * num2;
+result2 = num1 - num2;
+result3 = num1 / num2;
+print(result1);
+print(result2);
+print(result3);
+
+str1 = "안녕";
+str2 = "세상아";
+
+strResult = str1 + " " + str2;
+print(strResult);
---
+++ python/01/python01/first/TestInput.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2021. 10. 29.
+
+@author: USER
+'''
+num1 = input("입력1 : ");
+num2 = input("입력2 : ");
+
+intNum1 = int(num1);
+intNum2 = int(num2);
+
+result = intNum1 + intNum2;
+print(result);
